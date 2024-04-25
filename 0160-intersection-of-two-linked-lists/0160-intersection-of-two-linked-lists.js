44a9abe3ec1cfdcdb441fc08600204c3bec0fd06
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-04-25 10:29:18 +0200

    Time: 78 ms (67.13%), Space: 58.2 MB (71.73%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+const getIntersectionNode = function(headA, headB) { // O(n) time | O(1) space
+    // mark list A
+    markList(headA);    
+    // get intersection node if exited
+    const intersectionNode = checkIntersectionNode(headB); 
+    // remove mark from list A
+    markList(headA);    
+
+    return intersectionNode;
+};
+
+// mark the list by make its values become the opisite sign
+const markList = function(head) {
+    currNode = head;
+    while (currNode) {
+        currNode.val *= -1;
+        currNode = currNode.next;
+    }
+}
+
+// get intersection node if it's exited
+const checkIntersectionNode = function(head) {
+    currNode = head;
+    while (currNode) {
+        if (currNode.val < 0)
+            return currNode;
+        currNode = currNode.next;
+    }
+    return null;
+}
