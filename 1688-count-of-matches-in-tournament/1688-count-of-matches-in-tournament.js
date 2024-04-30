f1921fac1851382fe43e0bd5b09abdb1d2263d14
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-04-30 23:54:02 +0300

    Time: 59 ms (18.69%), Space: 48.1 MB (96.60%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -3,13 +3,5 @@
  * @return {number}
  */
 const numberOfMatches = function(n) {
-    let teams = n
-      , matches = 0;
-
-    while (teams !== 1) {
-        matches += Math.floor(teams / 2);
-        teams = Math.ceil(teams / 2);
-    }
-
-    return matches;
+    return n - 1;
 };
