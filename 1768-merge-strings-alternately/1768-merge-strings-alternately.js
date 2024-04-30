7bcd2af019faa028a88a294b5a2e7a1495df3360
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-04-30 23:47:44 +0300

    Time: 51 ms (71.69%), Space: 48.9 MB (87.47%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -4,11 +4,14 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-        let newStr = '';
-        const maxLen = Math.max(word1.length, word2.length);
-        for (let i = 0; i < maxLen; i++) {
-            newStr += word1[i] ?? '';
-            newStr += word2[i] ?? '';
-        }
-        return newStr;
+    const len = Math.min(word1.length, word2.length);
+    let i = 0
+      , alternatingStr = '';
+
+    for (; i < len; i++)
+        alternatingStr += word1[i] + word2[i];
+    
+    alternatingStr += word1.slice(i) + word2.slice(i);
+
+    return alternatingStr;
 };
