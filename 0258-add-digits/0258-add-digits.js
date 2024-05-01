c527175ea890647b76525fccd929684e40315409
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-01 13:47:34 +0300

    Time: 79 ms (48.36%), Space: 53.8 MB (16.71%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -4,16 +4,7 @@
  */
 const addDigits = function(num) {
     while (num > 9) 
-        num = sumOfDigits(num);
+        num = [...`${num}`].reduce((acc, digit) => acc + +digit, 0);
 
     return num;
 };
-
-const sumOfDigits = function (n) {
-    let sum = 0;
-    while (n) {
-        sum += (n % 10);
-        n = Math.floor(n / 10);
-    }
-    return sum;
-}
