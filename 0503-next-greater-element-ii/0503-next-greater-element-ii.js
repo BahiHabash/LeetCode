583fac6fb2067bf6d5cf41bdf58f77fc03f30f7e
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-01 23:28:00 +0300

    Time: 81 ms (75.97%), Space: 56.4 MB (33.98%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.js
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.js
@@ -3,28 +3,18 @@
  * @return {number[]}
  */
 const nextGreaterElements = function(nums) {
-    const nextGreater = new Map();  // indexOfEl : nextGreaterOfEl
+    const len = nums.length;
+    const nextGreater = new Array(len).fill(-1);
     const stack = [];
 
-    for (const [i, n] of nums.entries()) {
-        while (stack.length && nums[stack.at(-1)] < n) {
-            nextGreater.set(stack.pop(), n);
-        }
-        stack.push(i);
+    for (let i = 0; i < len * 2; i++) {
+        const idx = i % len;
+
+        while (stack.length && nums[stack.at(-1)] < nums[idx])
+            nextGreater[stack.pop()] = nums[idx];
+            
+        stack.push(idx);
     }
 
-    for (const i of stack) {
-        for (let j = (i + 1) % nums.length; j !== i; j = (j + 1) % nums.length) {
-            if (nums[j] > nums[i]) {
-                nextGreater.set(i, nums[j]);
-                break;
-            }
-            else if (nextGreater.get(j) && nextGreater.get(j) > nums[i]) {
-                nextGreater.set(i, nextGreater.get(j));
-                break;
-            }
-        }
-    }
-
-    return nums.map((_, i) => nextGreater.get(i) ?? -1);
+    return nextGreater;
 };
