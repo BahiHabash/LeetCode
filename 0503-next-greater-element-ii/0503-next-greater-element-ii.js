fd43f08325c1fba7b0a0905fc4499a5a95aed248
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-01 23:02:12 +0300

    Time: 715 ms (5.10%), Space: 60.2 MB (5.10%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.js
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.js
@@ -2,20 +2,29 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const nextGreaterElements = function(nums) { // O(n ^ 2)
-    return nums.map((n, i) => nextGreater(n, i, nums));
-};
+const nextGreaterElements = function(nums) {
+    const nextGreater = new Map();  // indexOfEl : nextGreaterOfEl
+    const stack = [];
 
-/**
-* @param {number} n
-* @param {number} index
-* @param {number[]} nums
- */
-const nextGreater = function(n, index, nums) {
-    const len = nums.length; 
-    for (let i = (index + 1) % len; i !== index; i = (i + 1) % len) {
-        if (nums[i] > n)
-            return nums[i];
+    for (const [i, n] of nums.entries()) {
+        while (stack.length && nums[stack.at(-1)] < n) {
+            nextGreater.set(stack.pop(), n);
+        }
+        stack.push(i);
     }
-    return -1;
-}
+
+    for (const i of stack) {
+        for (let j = (i + 1) % nums.length; j !== i; j = (j + 1) % nums.length) {
+            if (nums[j] > nums[i]) {
+                nextGreater.set(i, nums[j]);
+                break;
+            }
+            else if (nextGreater.get(j) && nextGreater.get(j) > nums[i]) {
+                nextGreater.set(i, nextGreater.get(j));
+                break;
+            }
+        }
+    }
+
+    return nums.map((_, i) => nextGreater.get(i) ?? -1);
+};
