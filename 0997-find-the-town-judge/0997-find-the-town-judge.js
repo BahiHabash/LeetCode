23d738407011df402960086f6b4d4c063a2d3463
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-01 04:38:13 +0300

    refactor code

--- 0997-find-the-town-judge/0997-find-the-town-judge.js
+++ 0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -4,25 +4,33 @@
  * @return {number}
  */
 const findJudge = function(n, trust) {
-    if(trust.length === 0) {
+    if(trust.length === 0)     // handle when trust is empty array
         return n === 1 ? 1 : -1;
-    }
     
-    const trustMap = new Map();
+    const trustMap = new Map();// person : set of people he trusts on
     trust.forEach(rel => trustMap.set(rel[0], (trustMap.get(rel[0]) || new Set()).add(rel[1])));
     
-    if (trustMap.size !== n - 1)
+    if (trustMap.size !== n - 1)// if not everybody trust on at least a person
         return -1;
     
     for(let i = 1; i <= n; i++) {
-        if (! trustMap.has(i)) {
-            for (const trustedPeople of trustMap.values()) {
-                if (! trustedPeople.has(i))
-                    return -1;
-            }
-            return i;
+        if (! trustMap.has(i)) {// may be a judge
+            return isTrustedByAll(trustMap, i) ? 1 : -1;
         }
     }
     
     return -1;
 };
+
+/**
+ * @param {map} people
+ * @param {number} person
+ * @return {boolean}
+ */
+const isTrustedByAll = function(people, person) {
+    for (const trustedPeople of people.values()) {
+        if (! trustedPeople.has(person))
+            return false;
+    }
+    return true;
+}
