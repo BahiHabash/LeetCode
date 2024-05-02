1985b655c8dfd27430c2005e2aea89b4f8c03eef
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-02 14:55:39 +0300

    Time: 67 ms (42.94%), Space: 49.5 MB (97.57%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,10 +3,5 @@
  * @return {number}
  */
 const singleNumber = function(nums) { // Time : O(n * log(n)) , Space : O(1)
-    const uniqueNums = [...new Set(nums)]
-    
-    const summition = nums.reduce((acc, n) => acc + n, 0);
-    const desiredSummition = uniqueNums.reduce((acc, n) => acc + n, 0) * 2;
-    
-    return desiredSummition - summition;
+    return nums.reduce((acc, num) => acc ^ num, 0);
 };
