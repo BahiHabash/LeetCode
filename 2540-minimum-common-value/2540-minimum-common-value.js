2f45297e67a187e5a1260c308f79cd9ac67c09a2
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-05 21:33:03 +0300

    Time: 74 ms (39.88%), Space: 59.2 MB (32.54%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.js
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -3,26 +3,31 @@
  * @param {number[]} nums2
  * @return {number}
  */
-const getCommon = function(nums1, nums2) { // Time : O(n) | Space : O(1)
-    let i = 0
-      , j = 0;
-    
-    while (inBoundries()) {
-        while (inBoundries() && nums1[i] > nums2[j]) 
-            j++;
-
-        if (nums1[i] === nums2[j])
-            return nums1[i];
-
-        i++;
-    }
-
-    function inBoundries(){
-        return (i < nums1.length) && (j < nums2.length);
-    }
+const getCommon = function(nums1, nums2) { // Time : O(n * log(n)) | Space : O(1)
+    for (const n of nums1)
+        if (isExisted(nums2, n))
+            return n;
 
     return -1;
 };
+// do binary search for element in an array
+const isExisted = function (arr, n) {
+    let l = 0
+      , r = arr.length - 1;
+    
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2);
+
+        if (arr[mid] === n)
+            return true;
+        if (arr[mid] > n)
+            r = mid - 1;
+        else
+            l = mid + 1;
+    }
+
+    return false;
+}
 
 
 
