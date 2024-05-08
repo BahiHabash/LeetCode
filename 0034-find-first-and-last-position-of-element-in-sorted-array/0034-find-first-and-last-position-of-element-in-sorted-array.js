36a57e1a19303f79e815a4a044f857135825302a
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-08 21:30:42 +0300

    Time: 39 ms (99.01%), Space: 50 MB (16.21%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,33 +4,27 @@
  * @return {number[]}
  */
 const searchRange = function(nums, target) {
-    return [getBoundry(nums, target, 'lowwer'), getBoundry(nums, target, 'upper')];
+    return [lowwerBoundry(nums, target), upperBoundry(nums, target)];
 };
 
-const getBoundry = function(nums, target, boundry) {
-    function checkBoundry(boundry, i) {
-        if (boundry === 'upper')
-            return (nums[i + 1] ?? Infinity) !== target;
-        else if (boundry === 'lowwer')
-            return (nums[i - 1] ?? Infinity) !== target;
-    }
-
-    let l = 0, r = nums.length - 1;
-
+const lowwerBoundry = function(nums, target) {
+    let idx = -1, l = 0, r = nums.length - 1;
     while (l <= r) {
         const mid = Math.floor((l + r) / 2);
-        
-        if ((nums[mid] === target)){
-            if (checkBoundry(boundry, mid))
-                return mid;
-            else 
-                (boundry === 'upper') ? l = mid + 1 : r = mid - 1;
-        }
-        else if (nums[mid] > target) 
-            r = mid - 1;
-        else
-            l = mid + 1;
+        if (nums[mid] === target) idx = mid;
+        if (nums[mid] >= target) r = mid - 1;
+        else l = mid + 1;
     }
+    return idx;
+};
 
-    return -1;
-}
+const upperBoundry = function(nums, target) {
+    let idx = -1, l = 0, r = nums.length - 1;
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2);
+        if (nums[mid] === target) idx = mid;
+        if (nums[mid] <= target) l = mid + 1;
+        else r = mid - 1;
+    }
+    return idx;
+};
