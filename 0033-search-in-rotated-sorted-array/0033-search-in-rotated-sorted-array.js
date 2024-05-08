1a66faf50202fb5ff154a3f3d28be464f7385f30
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-08 19:55:37 +0300

    Time: 64 ms (8.04%), Space: 50.6 MB (6.90%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -3,21 +3,10 @@
  * @param {number} target
  * @return {number}
  */
-const search = function(nums, target) { // Time : O(n * log(n)) | Space : O(n)
-    const map = new Map();
-    nums.forEach((n, i) => map.set(n, i));
-    return isExisted([...nums].sort((a, b) => a - b), target) ? map.get(target) : -1;
-};
-
-const isExisted = function(nums, target) {
-    let l = 0, r = nums.length - 1;
-
-    while (l <= r) {
-        const mid = Math.floor((l + r) / 2);
-        if (nums[mid] === target) return true;
-        else if (nums[mid] > target) r = mid -1;
-        else l = mid + 1;
+const search = function(nums, target) { // Time : O(n) | Space : O(1)
+    for (const [i, num] of nums.entries()) {
+        if (num === target)
+            return i;
     }
-
-    return false;
-}
+    return -1;
+};
