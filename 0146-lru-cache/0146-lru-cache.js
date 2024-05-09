66ba1059ba981fa7011a7fca6920029d1d8788ce
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-09 03:27:22 +0300

    Time: 477 ms (70.85%), Space: 105.2 MB (64.53%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.js
+++ 0146-lru-cache/0146-lru-cache.js
@@ -1,116 +1,34 @@
-class ListNode {
-    /**
-     * 
-     * @param {Number} key 
-     * @param {Number} val 
-     * @param {ListNode} prev 
-     * @param {ListNode} next 
-     */
-    constructor(key, val, prev = null, next = null) {
-        this.key = key;
-        this.val = val;
-        this.prev = prev;
-        this.next = next;
-    }
-}
-
-class DLL {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
-    /**
-     * 
-     * @param {Number} key 
-     * @param {Number} val 
-     * @returns {ListNode}
-     */
-    push(key, val) {
-        const newNode = new ListNode(key, val);
-        if (this.length === 0) {   // there no nodes in the list
-            this.head = newNode;
-            this.tail = newNode;
-        }
-        else {                  // there is nodes in the list
-            this.tail.next = newNode;
-            newNode.prev = this.tail;
-            this.tail = newNode;
-        }
-        this.length++;
-        return newNode;
-    }
-    /**
-     * @param {ListNode} node 
-     * @returns {number}
-     */
-    remove(node) {
-        if (this.length === 0) 
-            return;
-        else if (this.length === 1) {       // there is only one node in the list
-            this.head = null;
-            this.tail = null;
-        }
-        else if (node === this.head) { // the node is the head
-            this.head = this.head.next;
-            this.head.prev = null;
-        }
-        else if (node === this.tail) { // the node is the tail
-            this.tail = node.prev;
-            this.tail.next = null;
-        }
-        else {                        // the node is in the middle
-            const prev = node.prev;
-            const next = node.next;
-            prev.next = next;
-            next.prev = prev;
-        }
-        return --this.length;
-    }
-}
-
 class LRUCache {
     /**
-    * @param {Number} capasity
+    * @param {Number} capacity
     */
-    constructor(capasity = 0) {
-        this.capasity = capasity;
-        this.list = new DLL();
-        this.cash = {};
-        this.size = 0;
+    constructor(capacity = 0) {
+        this.capacity = capacity;
+        this.cache = new Map();
     }
     /**
     * @param {Number} key
-    * @return {Number}
+    * @return {Number}  Value of the Key if existed or -1 if not
     */
     get(key) {
-        if (! this.cash[key])
-            return -1;
+        if (! this.cache.has(key)) return -1;
 
-        const value = this.cash[key].val;
-        this.list.remove(this.cash[key]);
-        this.cash[key] = this.list.push(key, value);
-
+        const value = this.cache.get(key);
+        this.cache.delete(key);
+        this.cache.set(key, value);
         return value;
     }
     /**
-    * @param {Number} key
-    * @param {Number} val
+    * @param {Number} key  key to be added
+    * @param {Number} val  value of the key
     */
     put(key, val) {
-        if (this.cash[key])    // if the key is existed
-            this.list.remove(this.cash[key]);
-
-        else {
-            if (this.size === this.capasity) {
-                const removedKey = this.list.head.key; // LRU Cash key
-                this.list.remove(this.list.head);
-                delete this.cash[removedKey];
-            }
-            else
-                this.size++;
-        }
-
-        this.cash[key] = this.list.push(key, val); 
+        if (this.cache.has(key))
+            this.cache.delete(key);
+        
+        this.cache.set(key, val);
+        
+        if (this.cache.size > this.capacity) 
+            this.cache.delete(this.cache.keys().next().value);
     }
 }
