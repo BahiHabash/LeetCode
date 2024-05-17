967cdfd86d57604cdb8cfded5e58a4e32c51dd08
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-17 23:05:23 +0300

    Time: 76 ms (44.46%), Space: 57.7 MB (34.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -2,49 +2,54 @@
  * @param {character[][]} grid
  * @return {number}
  */
-function numIslands(grid) { // Space : O(1)
+function numIslands(grid) { // BFS, Time : O(n * m), Space : O(1)
+    // Handle edge cases (empty and null grid)
+    if ((!grid) || (grid.length === 0) || (grid[0].length === 0))
+        return 0;
+    
+    let islandsCount = 0;
     const m = grid.length;
     const n = grid[0].length;
-    let count = 0;
+    const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]];
 
-    /**
-     * @param {number} i
-     * @param {number} j
-     * @return {boolean}
+    /** Check if a give cell's indeces in boundry of the grid size
+     * @param {number} i row index
+     * @param {number} j column index
+     * @return {boolean} True if cell is in boundry of grid, False otherwise
      */
     function inBoundry(i, j) {
         return (i >= 0) && (j >= 0) && (i < m) && (j < n);
     }
 
-    /**
-     * @param {number} i
-     * @param {number} j
+    /** Perform BFS on the grid
+     * @param {number} i starting row index
+     * @param {number} j starting column index
      */
-    function dfs(i, j) {
-    grid[i][j] = '0';
-        const directions = [
-            [0, 1],  // right
-            [0, -1], // left
-            [1, 0],  // up
-            [-1, 0], // down
-        ]
-
-        for(let [ni, nj] of directions) {
-            ni += i;
-            nj += j;
-            if (inBoundry(ni, nj) && grid[ni][nj] === '1')
-                dfs(ni, nj);
-        }
-    }
-
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] === '1') {
-                dfs(i, j);
-                count++;
+    function BFS(i, j) {
+        const queue = [[i, j]];
+        grid[i][j] = '0';
+        while (queue.length) {
+            const [r, c] = queue.shift();
+            
+            for (let [ni, nj] of directions) {
+                ni += r, nj += c;
+                if (inBoundry(ni, nj) && grid[ni][nj] === '1'){
+                    grid[ni][nj] = '0';
+                    queue.push([ni, nj]);
+                }
             }
         }
     }
 
-    return count;
+    // Iterate over each cell of the grid
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === '1') {
+                BFS(i, j);
+                islandsCount++;
+            }
+        }
+    }
+
+    return islandsCount;
 }
