610da39740ea8d42ccdf1624082ffd9e4587afc1
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-17 21:58:20 +0300

    Time: 103 ms (17.91%), Space: 63.6 MB (9.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -2,43 +2,50 @@
  * @param {character[][]} grid
  * @return {number}
  */
-const numIslands = function(grid) {
-    const m = grid.length
-        , n = grid[0].length
-        , visited = new Set();
+function numIslands(grid) {
+    const visited = new Array(300).fill(null).map(() => new Array(300).fill(false));
+    const m = grid.length;
+    const n = grid[0].length;
     let count = 0;
-    
+
     /**
      * @param {number} i
      * @param {number} j
      * @return {boolean}
      */
-    const inBoundry = function(i, j) {
+    function inBoundry(i, j) {
         return (i >= 0) && (j >= 0) && (i < m) && (j < n);
-    };
-    
+    }
+
     /**
-     * @param {character[][]} grid
      * @param {number} i
      * @param {number} j
      */
-    const dfs = function(grid, i, j) {
-        visited.add(`${i},${j}`);
-        if (inBoundry(i, j + 1) && !visited.has(`${i},${j + 1}`) && grid[i][j + 1] === '1') dfs(grid, i, j + 1);
-        if (inBoundry(i, j - 1) && !visited.has(`${i},${j - 1}`) && grid[i][j - 1] === '1') dfs(grid, i, j - 1);
-        if (inBoundry(i + 1, j) && !visited.has(`${i + 1},${j}`) && grid[i + 1][j] === '1') dfs(grid, i + 1, j);
-        if (inBoundry(i - 1, j) && !visited.has(`${i - 1},${j}`) && grid[i - 1][j] === '1') dfs(grid, i - 1, j);
-    };
+    function dfs(i, j) {
+        visited[i][j] = true;
+        const directions = [
+            [0, 1],  // right
+            [0, -1], // left
+            [1, 0],  // up
+            [-1, 0], // down
+        ]
+
+        for(let [ni, nj] of directions) {
+            ni += i;
+            nj += j;
+            if (inBoundry(ni, nj) && !visited[ni][nj] && grid[ni][nj] === '1')
+                dfs(ni, nj);
+        }
+    }
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (!visited.has(`${i},${j}`) && grid[i][j] === '1'){
-                dfs(grid, i, j);
+            if (!visited[i][j] && grid[i][j] === '1') {
+                dfs(i, j);
                 count++;
             }
         }
     }
 
-
     return count;
-};
+}
