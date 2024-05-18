d2bd590ce727a306222056d0c6e760f5e3166483
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-18 14:21:59 +0300

    Time: 72 ms (56.03%), Space: 52.3 MB (52.59%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -2,12 +2,11 @@
  * @param {number[][]} grid
  * @return {number} Maximum area of an island in the grid
  */
-const maxAreaOfIsland = function(grid) { // Time : O(n * m) | Space : O(n * m)
+const maxAreaOfIsland = function(grid) { // Time : O(n * m) | Space : O(1)
     let maxIslandArea = 0;
     const m = grid.length;
     const n = grid[0].length;
     const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-    const visited = new Array(m).fill(null).map(() => new Array(n).fill(false));
 
     /** Check if the cell's indices within the grid's boundies
      * @param {number} row      row index
@@ -25,10 +24,10 @@ const maxAreaOfIsland = function(grid) { // Time : O(n * m) | Space : O(n * m)
      */
     const DFS = function(r, c) {
         let currIslandArea = 1;
-        visited[r][c] = true;
+        grid[r][c] = 0;
         for (let [nr, nc] of directions) {
             nr += r, nc += c;
-            if (isInBoundry(nr, nc) && grid[nr][nc] === 1 && !visited[nr][nc]) {
+            if (isInBoundry(nr, nc) && grid[nr][nc] === 1) {
                 currIslandArea += DFS(nr, nc);
             }
         }
@@ -38,7 +37,7 @@ const maxAreaOfIsland = function(grid) { // Time : O(n * m) | Space : O(n * m)
     // Iterate over the grid's cells
     for (let r = 0; r < m; r++) {
         for (let c = 0; c < n; c++) {
-            if ((grid[r][c] === 1) && (!visited[r][c]))
+            if (grid[r][c] === 1)
                 maxIslandArea = Math.max(DFS(r, c), maxIslandArea);
         }
     }
