b9d2e4e1b57de5775798a0990cb221ba41139a37
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-20 09:05:09 +0300

    Time: 82 ms (53.57%), Space: 59.5 MB (36.90%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -3,12 +3,11 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-const rotate = function(nums, k) { // Time : O(n), Space (n) 
+const rotate = function(nums, k) { // Time : O(n), Space (n)
     const len = nums.length;
-    const map = new Map(nums.entries());
     k = len - (k % len);
+    const numsOriginal = [...nums];
     for (let i = 0; i < nums.length; i++) {
-        nums[i] = map.get(k % len);
-        k++;
+        nums[i] = numsOriginal[k++ % len];
     }
 };
