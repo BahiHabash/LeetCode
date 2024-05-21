7ae69f0c2aebfbedeeeaf56eefeb18e209d8984b
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-21 22:45:23 +0300

    Time: 84 ms (83.31%), Space: 63.1 MB (89.91%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -3,12 +3,19 @@
  * @param {number} k
  * @return {boolean}
  */
-const containsNearbyDuplicate = function(nums, k) { // Time : O(n ^ 2), Space : O(1)
-    for (let i=0; i <= nums.length; i++) {
-        for (let j=i+1; j <= i+k && j < nums.length; j++) {
-            if (nums[i] === nums[j])
-                return true;
-        }
+const containsNearbyDuplicate = function(nums, k) { // Time : O(n), Space : O(k)
+    const seen = new Set();
+    for (let left = 0, right = 0; right < nums.length; right++) {
+        const currNum = nums[right];
+
+        if (seen.has(currNum))
+            return true;
+        
+        seen.add(currNum);
+
+        if (seen.size > k) 
+            seen.delete(nums[left++]);
     }
+
     return false;
 };
