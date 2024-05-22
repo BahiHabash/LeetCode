4f400ca9a2fc9569d3416c591aa3954bc3a13ccc
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-23 00:33:25 +0300

    Time: 75 ms (72.06%), Space: 61.1 MB (52.64%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -2,10 +2,10 @@
  * @param {number[]} nums
  * @return {number}
  */
-const findDuplicate = function(nums) { // Time : O(n * log(n)), Space : O(1)
-    nums.sort((a, b) => a - b);
-    for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[i] === nums[i + 1])
-            return nums[i];
+const findDuplicate = function(nums) { // Time : O(n), Space : O(1)
+    for (const num of nums) {
+        if (nums[Math.abs(num) - 1] < 0)
+            return Math.abs(num);
+        nums[Math.abs(num) - 1] *= -1;
     }
 };
