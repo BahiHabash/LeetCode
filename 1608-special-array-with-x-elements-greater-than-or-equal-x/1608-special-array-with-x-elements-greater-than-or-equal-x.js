582d3be1231b2e9cb5eeffc49541ff2afe7225e5
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-27 23:57:52 +0300

    Time: 55 ms (58.33%), Space: 48.5 MB (95.37%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.js
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.js
@@ -2,20 +2,12 @@
  * @param {number[]} nums
  * @return {number}
  */
-const specialArray = function(nums) { // Time : O(n ^ 2), Space : O(1)
-    let specialNum = -1;
-    
-    for(let i = 1; i <= nums.length; i++) {
-        let x = i;
-        let numOfNumsGreaterThanX = 0;
-        
-        for (let j = 0; j < nums.length; j++) {
-            numOfNumsGreaterThanX += x <= nums[j];
-        }
-        
-        if (x === numOfNumsGreaterThanX)
-            specialNum = x;
+const specialArray = function(nums) { // Time : O((n * log(n)) + (maxNumIn(nums) + nums.lnegth)), Space : O(1)
+    nums.sort((a, b) => a - b);
+    let idx = 0;
+    for(let i = 0; i <= nums.length; i++) {
+        while (i > nums[idx]) idx++;
+        if (nums.length - idx === i) return i;
     }
-    
-    return specialNum;
+    return -1;
 };
