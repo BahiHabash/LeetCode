75e09d834cce15577119c7c91c496285748494ec
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-28 04:31:37 +0300

    Time: 66 ms (44.04%), Space: 53.8 MB (11.93%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @param {number} maxCost
+ * @return {number}
+ */
+const equalSubstring = function(s, t, maxCost) { // Time : O(n), Space : O(n)
+    // sotre the difference between the (i)th chars in s and t
+    const charsDiffArr = [];
+    for (let i = 0; i < t.length; i++) {
+        charsDiffArr.push(Math.abs(s[i].charCodeAt(0) - t[i].charCodeAt(0)));
+    }
+
+    let maxLen = 0;
+    let currCost = 0;
+    for (let l = 0, r = 0; r < t.length; r++) {
+        // expand the window 
+        currCost += charsDiffArr[r];
+        // shrink the window when the currCost exeeded maxCost allowed
+        while (currCost > maxCost) {
+            currCost -= charsDiffArr[l++];
+        }
+        // update maxLen of subarray with cost less than or equal to maxCost so far
+        maxLen = Math.max((r - l + 1), maxLen);
+    }
+
+    return maxLen;
+};
