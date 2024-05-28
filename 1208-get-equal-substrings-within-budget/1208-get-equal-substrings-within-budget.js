f4a5e203940d753af501ba67edd173ea98430ae1
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-28 04:37:55 +0300

    Time: 72 ms (20.18%), Space: 52.2 MB (24.77%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.js
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.js
@@ -4,25 +4,28 @@
  * @param {number} maxCost
  * @return {number}
  */
-const equalSubstring = function(s, t, maxCost) { // Time : O(n), Space : O(n)
-    // sotre the difference between the (i)th chars in s and t
-    const charsDiffArr = [];
-    for (let i = 0; i < t.length; i++) {
-        charsDiffArr.push(Math.abs(s[i].charCodeAt(0) - t[i].charCodeAt(0)));
-    }
-
+const equalSubstring = function(s, t, maxCost) { // Time : O(n), Space : O(1)
     let maxLen = 0;
     let currCost = 0;
     for (let l = 0, r = 0; r < t.length; r++) {
         // expand the window 
-        currCost += charsDiffArr[r];
+        currCost += calcCharsDiff(s[r], t[r]);
         // shrink the window when the currCost exeeded maxCost allowed
         if (currCost > maxCost) {
-            currCost -= charsDiffArr[l++];
+            currCost -= calcCharsDiff(s[l], t[l]);
+            l++;
         }
         // update maxLen of subarray with cost less than or equal to maxCost so far
         maxLen = Math.max((r - l + 1), maxLen);
     }
 
     return maxLen;
+};
+/**
+ * @param {string} char1
+ * @param {string} char2
+ * @return {number} The absolute difference between the ASCII values of the characters
+ */
+const calcCharsDiff = function(char1, char2) {
+    return Math.abs(char1.charCodeAt(0) - char2.charCodeAt(0));
 };
