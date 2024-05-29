2e28d8a20a63a7970cc5e3e16d02a79ed965d660
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-29 06:04:42 +0300

    Time: 49 ms (88.89%), Space: 51.2 MB (12.96%) - LeetHub

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const numSteps = function(s) {
+    let num = BigInt(`0b${s}`);
+    let steps = 0;
+    while (num !== 1n) {
+        if (num % 2n === 0n) num /= 2n;
+        else num += 1n;
+        steps++;
+    }
+    return steps
+};
