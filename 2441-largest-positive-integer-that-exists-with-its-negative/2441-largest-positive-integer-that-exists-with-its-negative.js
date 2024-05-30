0020e52e77e97aa4c819d745be6623a867110b59
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-30 09:50:10 +0300

    Time: 52 ms (96.74%), Space: 51.4 MB (60.00%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.js
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.js
@@ -2,14 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-const findMaxK = function(nums) { // time : O(n^2), space : O(1)
+const findMaxK = function(nums) { // time : O(n), space : O(n)
     let maxPositiveIntger = -1;
+    const negatives = new Set(nums.filter(n => n < 0));
 
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === 0)
-                maxPositiveIntger = Math.max(Math.abs(nums[i]), maxPositiveIntger); 
-        }
+        if (negatives.has(-1 * nums[i]))
+            maxPositiveIntger = Math.max(nums[i], maxPositiveIntger);
     }
 
     return maxPositiveIntger;
