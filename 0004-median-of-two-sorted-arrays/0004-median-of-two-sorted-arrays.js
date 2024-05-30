fd802a390a50fcb240b38247511abdde1d7eab49
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-30 21:02:53 +0300

    Time: 107 ms (27.65%), Space: 54.6 MB (71.87%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -1,22 +1,19 @@
-const findMedianSortedArrays = function(nums1, nums2) { // Time : O(n)
-    let i = 0, j = 0, curr = 0, prev = 0;
-    const totalLength = nums1.length + nums2.length;
-    const mid = Math.floor(totalLength / 2);
-
-    while (i + j <= mid) {
-        prev = curr;
-        if ((nums1[i] ?? Infinity) < (nums2[j] ?? Infinity)) {
-            curr = nums1[i];
-            i++;
-        } 
-        else {
-            curr = nums2[j];
-            j++;
-        }
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const findMedianSortedArrays = function(nums1, nums2) { // Time : O(n), Space : O(1)
+    const arr = [];
+    for (let i = 0, j = 0; i < nums1.length || j < nums2.length;) {
+        if ((nums1[i] ?? Infinity) < (nums2[j] ?? Infinity))
+            arr.push(nums1[i++]);
+        else 
+            arr.push(nums2[j++]);
     }
-
-    if (totalLength % 2 === 0)
-        return (prev + curr) / 2;
-    else
-        return curr;
-};
+    const len = arr.length;
+    if (len % 2 === 0)
+        return (arr[len / 2] + arr[(len / 2) - 1]) / 2;
+    else 
+        return arr[Math.floor(len / 2)];
+}   
