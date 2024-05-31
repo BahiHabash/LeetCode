8c9a86aff0f81ffa759630250d2e2cfd9405e172
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-31 07:29:54 +0300

    Time: 99 ms (73.02%), Space: 64.3 MB (50.03%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,15 +2,20 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const productExceptSelf = function(nums) { // Time : O(n), Space : O(1)
-    const productArr = new Array(nums.length).fill(0);
-    const zeros = nums.reduce((acc, n) => acc + (n === 0), 0);
-    const productWithoutZeros = nums.reduce((acc, n) => acc * (n === 0 ? 1 : n), 1);
+const productExceptSelf = function(nums) { // Time : O(n), Space ; O(n)
+    const productArr = new Array(nums.length).fill(1);
 
-    if (zeros === 0)
-        productArr.forEach((n, i, pArr) => pArr[i] = (productWithoutZeros / nums[i]));
-    else if (zeros === 1)
-        productArr.forEach((n, i, pArr) => pArr[i] = (nums[i] === 0 ? productWithoutZeros : 0));
+    let frontProduct = 1;
+    for (let i = 0; i < nums.length; i++) {
+        productArr[i] *= frontProduct;
+        frontProduct *= nums[i];
+    }
+
+    let backProduct = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        productArr[i] *= backProduct;
+        backProduct *= nums[i];
+    }
 
     return productArr;
 };
