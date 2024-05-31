5799ccd2874a89d6ff0fe18d9ec35e0dc853a590
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-05-31 05:28:41 +0300

    Time: 69 ms (32.28%), Space: 52.6 MB (42.72%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.js
+++ 0260-single-number-iii/0260-single-number-iii.js
@@ -2,20 +2,15 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const singleNumber = function(nums) { // Time : O(n), Space : O(1)
+const singleNumber = function(nums) { // Time : O(n), Space : O(n)
+    const numsFreq = new Map();
+    for (const n of nums)
+        numsFreq.set(n, (numsFreq.get(n) || 0) + 1);
+
     const elemsAppearOnce = [];
-    
-    for (let i = 0; i < nums.length; i++) {
-        let isDuplicated = false;
-        for (let j = 0; j < nums.length; j++) {
-            if (i !== j && nums[i] === nums[j]){
-                isDuplicated = true;
-                break;
-            }
-        }
-        if (! isDuplicated)
-            elemsAppearOnce.push(nums[i]);
-    }
-    
-    return elemsAppearOnce;    
+    for (const [key, times] of numsFreq)
+        if (times === 1)
+            elemsAppearOnce.push(key);
+
+    return elemsAppearOnce;
 };
