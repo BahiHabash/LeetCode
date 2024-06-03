8846a9be4ebaf9b12bf82b3f367dc183f37a5194
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-06-03 11:44:55 +0300

    Time: 70 ms (5.79%), Space: 50.6 MB (9.55%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -4,16 +4,10 @@
  * @return {boolean}
  */
 const isSubsequence = function(s, t) {
-  let rStr = t;                  // rStr for remaining string of t
-
-  for(let i=0; i<s.length; i++){
-    const char = s[i];
-      
-    if(!rStr.includes(char))
-      return false;
-      
-    rStr = rStr.slice(rStr.indexOf(s[i]) + 1)
-  }
-
-  return true;
+    let i = 0;
+    for (const char of t) {
+        if (char === s[i]) i++;
+        if (i === s.length) return true;
+    }
+    return i === s.length;
 };
