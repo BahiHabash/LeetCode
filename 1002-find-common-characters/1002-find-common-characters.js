62041092b18fa980c88e397bf7dc355f0208f4e9
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-06-05 14:56:15 +0300

    Time: 82 ms (27.47%), Space: 57.9 MB (19.23%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.js
+++ 1002-find-common-characters/1002-find-common-characters.js
@@ -2,18 +2,21 @@
  * @param {string[]} words
  * @return {string[]}
  */
-const commonChars = function(words) { // Time : O(n * lengthOfWords), Space : O(1)
+const commonChars = function(words) { // Time : O(n * lengthOfWord), Space : O(1)
     let commonCharsMap = getCharsFreq(words[0]);
 
-    for (let i = 0; i < words.length; i++) {
-        const currCommonCharsMap = new Map();
-        wordCharsFreq = getCharsFreq(words[i]);
-        for (const [char, freq] of wordCharsFreq.entries()) {
+    for (let i = 1; i < words.length; i++) {
+        const wordCharsFreq = getCharsFreq(words[i]); // get word's char frequency
+
+        for (const [char, freq] of wordCharsFreq.entries()) { // set common chars with the least freq
             if (commonCharsMap.has(char))
-                currCommonCharsMap.set(char, Math.min(freq, commonCharsMap.get(char)));
+                commonCharsMap.set(char, Math.min(freq, commonCharsMap.get(char)));
         }
 
-        commonCharsMap = currCommonCharsMap;
+        for (const [char, freq] of commonCharsMap.entries()) { // delete non-common chars
+            if (!wordCharsFreq.has(char))
+                commonCharsMap.delete(char);
+        }
     }
 
     const res = [];
