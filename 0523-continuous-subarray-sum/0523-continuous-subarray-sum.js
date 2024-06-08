7297ef5030d0bd9bdf394d49999834fc6b45df9f
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-06-08 06:27:56 +0300

    Time: 138 ms (6.67%), Space: 72.5 MB (75.00%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
@@ -3,23 +3,20 @@
  * @param {number} k
  * @return {boolean}
  */
-const checkSubarraySum = function(nums, k) { // Time : O(n * (sum / k)), space : O(n)
+const checkSubarraySum = function(nums, k) { // Time : O(n), space : O(n)
     const prefixSum = new Map([[0, -1]]);
     let sum = 0; 
     for (let i = 0; i < nums.length; i++) {
         sum += nums[i];
-        if (! prefixSum.has(sum))
-            prefixSum.set(sum, i);
+        if (! prefixSum.has(sum % k))
+            prefixSum.set(sum % k, i);
     }
 
     let currSum = 0;
     for (let i = 0; i < nums.length; i++) {
         currSum += nums[i];
-        for (let j = currSum % k; j <= currSum; j += k) {
-            if ((i - (prefixSum.get(j) ?? Infinity) >= 2)
-             || (i - (prefixSum.get(currSum) ?? Infinity) >= 2))
-                return true;
-        }
+        if (i - (prefixSum.get(currSum % k) ?? Infinity) >= 2)
+            return true;
     }
 
     return false;
