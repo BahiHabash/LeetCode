f2e10ca562172ce45b2632a88594023ea0281262
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-06-11 12:38:49 +0300

    Time: 69 ms (9.55%), Space: 49.5 MB (55.91%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.js
+++ 1122-relative-sort-array/1122-relative-sort-array.js
@@ -3,35 +3,30 @@
  * @param {number[]} arr2
  * @return {number[]}
  */
-// time : O(m + n * log(n)), Space : O(n + m) | n => size of arr1, m => size of arr2
-const relativeSortArray = function(arr1, arr2) { 
-    const res = [];
-    const numsNotInArr2 = [];
-    const map = new Map(); // {n : freq}
+// time : O(n + m), Space : O(1001) | n => size of arr1, m => size of arr2
+const relativeSortArray = function(arr1, arr2) {
+    const freqArr = new Array(1001).fill(0); // {n : freq}
 
-    // fill frequency map for arr1 elements
+    // fill frequency array for arr1 elements
     for (const n of arr1) 
-        map.set(n, (map.get(n) ?? 0) + 1);
+        freqArr[n]++;
     
+    let i = 0;
     // sort the intersected elements between arr1 & arr2 and store it in res
     for (const n of arr2) {
-        let freq = map.get(n);
-        while (freq--)
-            res.push(n);
-        map.delete(n);
+        while (freqArr[n] > 0) {
+            arr1[i++] = n;
+            freqArr[n]--;
+        }
     }
 
-    // store the non-intersected elements in numsNotInArr2
-    for (let [n, freq] of map.entries()) {
-        while (freq--)
-            numsNotInArr2.push(n);
+    // sort the non-intersected elements in numsNotInArr2
+    for (let n = 0; n < freqArr.length; n++) {
+        while (freqArr[n] > 0) {
+            arr1[i++] = n;
+            freqArr[n]--;
+        }
     }
 
-    // sort the non-intersected elements
-    numsNotInArr2.sort((a, b) => a - b);
-
-    // append the numsNotInArr2 to the result array
-    res.push(...numsNotInArr2);
-
-    return res;
+    return arr1;
 };
