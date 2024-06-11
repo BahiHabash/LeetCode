9e1ca8454a7e37d19f12780b29985ef84cbe53fb
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-06-11 11:32:48 +0300

    Time: 65 ms (22.27%), Space: 52.8 MB (5.46%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
+ */
+const relativeSortArray = function(arr1, arr2) { // time : O(n * log(n)), Space : O(n)
+    const map1 = new Map(); // {n : freq}
+    for (const n of arr1)
+        map1.set(n, (map1.get(n) ?? 0) + 1); 
+    
+    const map2 = new Map(); // {n : index}
+    for (const [i, n] of arr2.entries())
+        map2.set(n, i)
+
+    // add and sort Elements that do not appear in arr2 in ascending order.
+    const numsNotInArr2 = [];
+    for (let [n, freq] of map1) {
+        if (!map2.has(n)) {
+            while(freq--) 
+                numsNotInArr2.push(n);
+            map1.delete(n);
+        }
+    }
+    numsNotInArr2.sort((a, b) => a - b);
+
+    // add and sort intersected elements in arr1
+    arr1 = [];
+    for (let [n, freq] of map1) {
+        while(freq--) {
+            arr1.push(n);
+            map1.delete(n);
+        }
+    }
+    arr1.sort((a, b) => map2.get(a) - map2.get(b));
+
+    // merget two sorted arrayes
+    arr1.push(...numsNotInArr2);
+
+    return arr1;
+};
