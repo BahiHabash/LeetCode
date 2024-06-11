3c837ad7855dc6ab6428de54150707c0a4227151
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-06-11 23:28:54 +0300

    Time: 112 ms (79.95%), Space: 75.4 MB (6.61%) - LeetHub

--- 0648-replace-words/0648-replace-words.js
+++ 0648-replace-words/0648-replace-words.js
@@ -3,19 +3,62 @@
  * @param {string} sentence
  * @return {string}
  */
+// using Trie | Time : O(n * k + m * k) 
+// n => n of roots in dictionary, k => average length of root & word, m => n of words in sentence
 const replaceWords = function(dictionary, sentence) {
-    const roots = new Set(dictionary);
+    const trie = new Trie(); 
     const words = sentence.split(' ');
 
-    for (const [i, word] of words.entries()) {
-        for (let j = 0; j < word.length; j++) {
-            const currRoot = word.slice(0, j + 1);
-            if (roots.has(currRoot)) {
-                words[i] = currRoot;
-                break;
-            }
-        }
-    }
+    // sorte roots in the trie
+    for (const root of dictionary)
+        trie.insert(root);
+
+    // replace each word with the shortest root
+    for (let i = 0; i < words.length; i++)
+        words[i] = trie.getRoot(words[i]);
 
     return words.join(' ');
 };
+
+class TrieNode {
+    constructor() {
+        this.isEnd = false;
+        this.children = new Array(26).fill(null);
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let curr = this.root;
+        for (const char of word) {
+            const idx = char.charCodeAt(0) - 97;
+
+            if (curr.children[idx] === null) 
+                curr.children[idx] = new TrieNode();
+
+            curr = curr.children[idx];
+        }
+        curr.isEnd = true;
+    }
+
+    getRoot(word) {
+        let curr = this.root;
+        for (let i = 0; i < word.length; i++) {
+            const idx = word.charCodeAt(i) - 97;
+
+            if (curr.children[idx] === null)
+                return word;
+
+            curr = curr.children[idx];
+
+            if (curr.isEnd)
+                return word.slice(0, i + 1);
+                
+        }
+        return word;
+    }
+}
