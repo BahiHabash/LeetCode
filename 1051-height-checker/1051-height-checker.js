4179a18b0601c64c1469d940a10cc7f340e4e93e
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-06-11 20:35:45 +0300

    Time: 50 ms (77.46%), Space: 50 MB (18.61%) - LeetHub

--- 1051-height-checker/1051-height-checker.js
+++ 1051-height-checker/1051-height-checker.js
@@ -2,21 +2,16 @@
  * @param {number[]} heights
  * @return {number}
  */
-const heightChecker = function(heights) { // Bubble sort, Time : O(n^2), Space : O(n)
-    const expected = [...heights];
+const heightChecker = function(heights) { // Insertion sort, Time : O(n^2), Space : O(n)
+    const expected = [];
 
-    for (let i = 0; i < expected.length; i++) {
-        let sorted = true;
+    for (const height of heights) {
+        let idx = expected.length;
         
-        for (let j = 0; j < expected.length - i - 1; j++) {
-            if (expected[j] > expected[j + 1]) {
-                [expected[j], expected[j + 1]] = [expected[j + 1], expected[j]];
-                sorted = false;
-            }
-        }
-        
-        if (i === 0 && sorted)
-            return 0;
+        while (expected[idx - 1] > height)
+            expected[idx] = expected[--idx];
+            
+        expected[idx] = height;
     }
 
     return heights.reduce((acc, height, i) => acc + (height !== expected[i]), 0);
