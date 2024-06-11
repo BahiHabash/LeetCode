016d9083d779c45e69922a732f4f72ab35dbb148
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-06-11 21:17:29 +0300

    Time: 50 ms (77.46%), Space: 50.7 MB (18.09%) - LeetHub

--- 1051-height-checker/1051-height-checker.js
+++ 1051-height-checker/1051-height-checker.js
@@ -2,17 +2,25 @@
  * @param {number[]} heights
  * @return {number}
  */
-const heightChecker = function(heights) { // Insertion sort, Time : O(n^2), Space : O(n)
-    const expected = [];
+const heightChecker = function(heights) { // Quick sort, Time : O(n^2), avg(n * log(n)), Space : O(n)
+    const expected = quickSort(heights);
+    return heights.reduce((acc, height, i) => acc + (height !== expected[i]), 0);
+};
 
-    for (const height of heights) {
-        let idx = expected.length;
-        
-        while (expected[idx - 1] > height)
-            expected[idx] = expected[--idx];
-            
-        expected[idx] = height;
+const quickSort = function(arr) {
+    if (arr.length <= 1) // Base Case
+        return arr;
+    
+    const pivot = arr[0];
+    const lessThanPivot = [];
+    const GreaterThanPivot = [];
+    
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot)
+            lessThanPivot.push(arr[i]);
+        else
+            GreaterThanPivot.push(arr[i]);
     }
 
-    return heights.reduce((acc, height, i) => acc + (height !== expected[i]), 0);
+    return [...quickSort(lessThanPivot), pivot, ...quickSort(GreaterThanPivot)];
 };
