1dd72c4fe5b92f53da0001a57995ad24c21a664d
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-07-04 00:11:24 +0300

    Time: 121 ms (46.94%), Space: 56.7 MB (97.96%) - LeetHub

---
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minDifference = function(nums) {
+    if (nums.length <= 3) return 0;
+    
+    nums.sort((a, b) => a - b);
+
+    // all four passable scenarios 
+    return Math.min(
+                    nums.at(-1) - nums.at(3),
+                    nums.at(-2) - nums.at(2),
+                    nums.at(-4) - nums.at(0),
+                    nums.at(-3) - nums.at(1)
+                );
+};
