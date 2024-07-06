0b39175858e9b0319b825ec80ec158795bf0f297
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-07-06 05:32:54 +0300

    Time: 51 ms (68.29%), Space: 48.9 MB (20.73%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.js
+++ 2582-pass-the-pillow/2582-pass-the-pillow.js
@@ -3,19 +3,12 @@
  * @param {number} time
  * @return {number}
  */
-const passThePillow = function(n, time) { // time : O(time), space : O(1)
-    let position = 1
-      , direction = 'right';
-
-    // 1 -> 2 -> 3   , time = 2
-    for (let i = 1; i <= time; i++) {
-        // move position
-        if (direction === 'right') position++;
-        else position--;
-        // reverse the directions when hit the boundries
-        if (position === n) direction = 'left';
-        else if (position === 1) direction = 'right';
+const passThePillow = function(n, time) { // time : O(1), space : O(1)
+    const edges = n - 1;
+    
+    if (Math.floor(time / edges) % 2 === 1) {
+        return n - Math.abs(time % edges);
+    } else {
+        return 1 + Math.abs(time % edges);
     }
-
-    return position;
 };
