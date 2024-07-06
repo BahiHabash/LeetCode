2f408d57410f557b3c2f5349bb2fe04f2bc6d195
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-07-06 06:42:33 +0300

    Time: 55 ms (42.86%), Space: 49.1 MB (25.00%) - LeetHub

--- 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.js
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.js
@@ -3,11 +3,5 @@
  * @return {number}
  */
 const maximumValue = function(strs) { // time : O(n), space : O(1)
-    let maxValue = -Infinity;
-
-    for (const str of strs) {
-        maxValue = Math.max(isNaN(str) ? str.length : +str, maxValue);
-    }
-
-    return maxValue;
+    return strs.reduce((acc, str) => Math.max(isNaN(str) ? str.length : +str, acc), -Infinity);
 };
