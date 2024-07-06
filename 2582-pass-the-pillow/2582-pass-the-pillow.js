55dc784ab6679435f48708a93e852941eb3ca8f0
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-07-06 05:35:32 +0300

    Time: 43 ms (95.12%), Space: 48.5 MB (84.15%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.js
+++ 2582-pass-the-pillow/2582-pass-the-pillow.js
@@ -5,10 +5,5 @@
  */
 const passThePillow = function(n, time) { // time : O(1), space : O(1)
     const edges = n - 1;
-    
-    if (Math.floor(time / edges) % 2 === 1) {
-        return n - Math.abs(time % edges);
-    } else {
-        return 1 + Math.abs(time % edges);
-    }
+    return Math.floor(time / edges) % 2 === 1 ? n - time % edges : 1 + time % edges;
 };
