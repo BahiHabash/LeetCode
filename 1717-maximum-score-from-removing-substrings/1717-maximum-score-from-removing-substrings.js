1476dbdbbbb35ef7e43e0cc6f1ec97fa2940d243
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-07-12 12:37:27 +0300

    Time: 161 ms (16.67%), Space: 62.9 MB (33.33%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.js
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.js
@@ -5,71 +5,48 @@
  * @return {number}
  */
 const maximumGain = function(s, x, y) {
-    let X = {
-        val: x,
-        str: 'ab',
-    };
-    let Y = {
-        val: y,
-        str: 'ba',
-    };
+    let xStr = 'ab', yStr = 'ba';
 
     // always make x > y
     if (x < y) {
-        [X, Y] = [Y, X];
+        [x, y] = [y, x];
+        [xStr, yStr] = [yStr, xStr];
     }
 
-    let maxGain = 0
+    let maxGain = 0, stack = [];
+
+    for (const char of s) {        
+        if (stack.length && stack[stack.length - 1] + char === xStr) {
+            maxGain += x;
+            stack.pop();
+        } else if(xStr.includes(char)){
+            stack.push(char);
+        } else {
+            maxGain += calcGainY(stack, y, yStr)
+            stack = []; // reset stack to empty state
+        }
+    }
+
+    // Final gain calculation for remaining characters in the stack
+    if (stack.length) {
+        maxGain += calcGainY(stack, y, yStr);
+    }
+
+    return maxGain;
+};
+
+const calcGainY = function(chars, y, yStr) {
+    let maxYGain = 0
       , stack = [];
 
-    for (const char of s) {
-        if (char !== 'a' && char !== 'b' && stack.length) {
-            maxGain += calcMaxGain(stack, X, Y);
-            stack = [];
-        } else {
-            stack.push(char);
-        }
-    }
-
-    if (stack.length) {
-        maxGain += calcMaxGain(stack, X, Y);
-        stack = [];
-    }
-
-    return maxGain;
-};
-
-const calcMaxGain = function(arr, X, Y) {
-    let maxGain = 0;
-
-    if (arr.length <= 1) {
-        return maxGain;
-    }
-
-    let stack = [];
-
-    // calc 'ab' gain
-    for (const char of arr) {
-        if (stack.length && stack[stack.length - 1] + char === X.str) {
-            maxGain += X.val;
+    for (const char of chars) {
+        if ((stack.at(-1) ?? '') + char === yStr) {
             stack.pop();
+            maxYGain += y;
         } else {
             stack.push(char);
         }
     }
 
-    arr = stack;
-    stack = [];
-
-    // calc 'ba' gain
-    for (const char of arr) {
-        if (stack.length && stack[stack.length - 1] + char === Y.str) {
-            maxGain += Y.val;
-            stack.pop();
-        } else {
-            stack.push(char);
-        }
-    }
-
-    return maxGain;
-};
+    return maxYGain;
+}
