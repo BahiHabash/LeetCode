2add70d2b2ab90a978d1e170524227463eb98cc2
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-07-16 09:37:50 +0300

    Time: 210 ms (96.51%), Space: 91.5 MB (93.02%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.js
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.js
@@ -14,37 +14,41 @@
  */
 const getDirections = function(root, startValue, destValue) { // Time : O(n), Space : O(h) => h : hight of the tree
     const startValuePath = getPathOf(root, startValue);
-    const destValuePath = getPathOf(root, destValue);  
-    console.log(startValuePath, destValuePath)
-    
-    // pick parent of staring and destination node
-    let parentIndex = 0;
-    while (parentIndex < startValuePath.length && parentIndex < destValuePath.length &&
-           startValuePath[parentIndex] === destValuePath[parentIndex]
-          ) parentIndex++;
+    const destValuePath = getPathOf(root, destValue);
     
     // staring is the root of the tree
     if (!startValuePath) {
         return destValuePath;
     }
     // staring is parent of destination
-    else if (!destValuePath) {
+    if (!destValuePath) {
         return 'U'.repeat(startValuePath.length);
     }
+
+    // pick parent of staring and destination node
+    let parentIndex = 0;
+    while (startValuePath[parentIndex] === destValuePath[parentIndex]) {
+        parentIndex++;
+    } 
+
     // staring is parent of destination
-    else if (!startValuePath.slice(parentIndex)) {
+    if (!startValuePath.slice(parentIndex)) {
         return destValuePath.slice(parentIndex);
     } 
     // destination is parent of staring
-    else if (!destValuePath.slice(parentIndex)) {
+    if (!destValuePath.slice(parentIndex)) {
         return 'U'.repeat(startValuePath.length - parentIndex);
     } 
     // staring and destination are children of a common parent
-    else {
-        return 'U'.repeat(startValuePath.length - parentIndex) + destValuePath.slice(parentIndex);
-    }
+    return 'U'.repeat(startValuePath.length - parentIndex) + destValuePath.slice(parentIndex);
 };
 
+/** get root of a target node that have specific value starting from the root
+ * @param {TreeNode} root root of the tree
+ * @param {number} target target node's value
+ * @param {string} path current path of the target node starting from the root 
+ * @return {strign} the path of the target node starting from the root
+ */
 const getPathOf = function(root, target, path = '') {
     // target not in this path
     if (!root) return '';
