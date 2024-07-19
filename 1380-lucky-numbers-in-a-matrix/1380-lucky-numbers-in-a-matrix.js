09e7b9838875ee5383fccb049c9ed00fce37f236
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-07-19 23:50:24 +0300

    Time: 68 ms (26.00%), Space: 52.2 MB (29.00%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.js
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.js
@@ -2,28 +2,25 @@
  * @param {number[][]} matrix
  * @return {number[]}
  */
-const luckyNumbers  = function(matrix) {
-    const [m, n] =[ matrix.length, matrix[0].length];
-    
-    const minNumOfRows = [];
-    for (const row of matrix) {
-        minNumOfRows.push( row.reduce((acc, n) => Math.min(acc, n), Infinity) );
-    }
+const luckyNumbers  = function(matrix) { // Time : O(m * n), Space : O(m + n)
+    // get length of row (m) and length of columns(n)
+    const [m, n] = [ matrix.length, matrix[0].length];
 
-    const maxNumOfColumns = [];
-    for (let i = 0; i < n; i++) {
-        let maxNumOfCurrColumn = -Infinity;
-        for (let j = 0; j < m; j++) {
-            maxNumOfCurrColumn = Math.max(matrix[j][i], maxNumOfCurrColumn);
+    const minNumOfRows = new Array(m).fill(Infinity);
+    const maxNumOfColumns = new Array(n).fill(-Infinity);
+    // get min number of each rows and max number of each columns
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            minNumOfRows[i] = Math.min(matrix[i][j], minNumOfRows[i]);
+            maxNumOfColumns[j] = Math.max(matrix[i][j], maxNumOfColumns[j]);
         }
-        maxNumOfColumns.push(maxNumOfCurrColumn);
     }
 
     const luckyNums = [];
-
+    // get lucky numbers
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if ((matrix[i][j] === minNumOfRows[i]) && (matrix[i][j] === maxNumOfColumns[j])) {
+            if (minNumOfRows[i] === maxNumOfColumns[j]) { //is the (min num in its row) and (max in its column)
                 luckyNums.push(matrix[i][j]);
             }
         }
@@ -31,3 +28,4 @@ const luckyNumbers  = function(matrix) {
 
     return luckyNums;
 };
+
