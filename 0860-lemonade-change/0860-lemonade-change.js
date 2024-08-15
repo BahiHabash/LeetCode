dd41a5cb59b69a7583eceb9c73e85e5a548aef7e
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-08-15 10:44:12 +0300

    Time: 72 ms (64.97%), Space: 60.4 MB (12.69%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.js
+++ 0860-lemonade-change/0860-lemonade-change.js
@@ -3,29 +3,23 @@
  * @return {boolean}
  */
 const lemonadeChange = function(bills) {
-    const myChange = new Map([[5, 0], [10, 0], [20, 0]]);
+    const myChange = {5: 0, 10: 0, 20: 0};
 
     for (const bill of bills) {
-        if (bill === 5) {
-            myChange.set(5, myChange.get(5) + 1);
-        }
-        else if (bill === 10) {
-            myChange.set(10, myChange.get(10) + 1);
-            if (myChange.get(5) < 1) {
+        myChange[bill]++;
+        
+        if (bill === 10) {
+            if (!myChange[5]) return false;
+            myChange[5]--;
+        } else if (bill === 20) {
+            if (myChange[5] && myChange[10]) {
+                myChange[5]--;
+                myChange[10]--;
+            } else if (myChange[5] >= 3) {
+                myChange[5] -= 3;
+            } else { 
                 return false;
             }
-            myChange.set(5, myChange.get(5) - 1);
-        } else {
-            myChange.set(20, myChange.get(20) + 1);
-            if (myChange.get(5) >= 1 && myChange.get(10) >= 1) {
-                myChange.set(5, myChange.get(5) - 1);
-                myChange.set(10, myChange.get(10) - 1);
-            } else if (myChange.get(5) >= 3) {
-                myChange.set(5, myChange.get(5) - 3);
-            } else {
-                return false;
-            }
-
         }
     }
 
