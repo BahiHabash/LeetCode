faeb23b7dabb421ae942c9f07758a1f8ee3a3065
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-09-09 17:39:36 +0300

    Time: 551 ms (5.40%), Space: 91.8 MB (29.73%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.js
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {ListNode} head
+ * @return {number[][]}
+ */
+const spiralMatrix = function(m, n, head) { // Time : O(m * n)
+    const DEFAULT_VALUE = -1;
+    const mat = new Array(m).fill(null).map(() => new Array(n).fill(DEFAULT_VALUE));
+    
+    let currNode = head;
+    let [row, col] = [0, 0];
+
+    while (currNode) {
+        // right 
+        while ((mat?.[row]?.[col] === DEFAULT_VALUE) && currNode) {
+            mat[row][col++] = currNode.val;
+            currNode = currNode.next;
+        }
+        
+        row++, col--;
+
+        // down 
+        while ((mat?.[row]?.[col] === DEFAULT_VALUE) && currNode) {
+            mat[row++][col] = currNode.val;
+            currNode = currNode.next;
+        }
+
+        row--, col--;
+
+        // left
+        while ((mat?.[row]?.[col] === DEFAULT_VALUE) && currNode) {
+            mat[row][col--] = currNode.val;
+            currNode = currNode.next;
+        }
+
+        row--, col++;
+
+        // up 
+        while ((mat?.[row]?.[col] === DEFAULT_VALUE) && currNode) {
+            mat[row--][col] = currNode.val;
+            currNode = currNode.next;
+        }
+
+        row++, col++;
+    }
+
+    return mat;
+};
