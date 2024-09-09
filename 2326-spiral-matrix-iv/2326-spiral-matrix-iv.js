8066ddce1922690e882c34fe5c358412ed613796
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-09-09 18:12:34 +0300

    Time: 494 ms (16.21%), Space: 88.1 MB (59.46%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.js
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.js
@@ -12,41 +12,22 @@
  * @return {number[][]}
  */
 const spiralMatrix = function(m, n, head) { // Time : O(m * n)
-    const DEFAULT_VALUE = -1;
-    const mat = Array.from({ length: m }, () => new Array(n).fill(DEFAULT_VALUE));
-;
-    
-    let currNode = head;
-    let [row, col] = [0, 0];
+    const DEFAULT_VALUE = -1
+        , DIRECTIONS = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        , mat = Array.from({ length: m }, () => new Array(n).fill(DEFAULT_VALUE));
+    let [i, j, curr_dir, currNode] = [0, 0, 0, head];
 
     while (currNode) {
-        // right 
-        while ((mat?.[row]?.[col] === DEFAULT_VALUE) && currNode) {
-            mat[row][col++] = currNode.val;
-            currNode = currNode.next;
-        }
-        row++, col--;
+        mat[i][j] = currNode.val;
+        let [newI, newJ] = [i + DIRECTIONS[curr_dir][0], j + DIRECTIONS[curr_dir][1]];
 
-        // down 
-        while ((mat?.[row]?.[col] === DEFAULT_VALUE) && currNode) {
-            mat[row++][col] = currNode.val;
-            currNode = currNode.next;
+        if (newI < 0 || newJ < 0 || newI >= m || newJ >= n || mat[newI][newJ] !== DEFAULT_VALUE) {
+            curr_dir = (curr_dir + 1) % DIRECTIONS.length;
+            [newI, newJ] = [i + DIRECTIONS[curr_dir][0], j + DIRECTIONS[curr_dir][1]];
         }
-        row--, col--;
 
-        // left
-        while ((mat?.[row]?.[col] === DEFAULT_VALUE) && currNode) {
-            mat[row][col--] = currNode.val;
-            currNode = currNode.next;
-        }
-        row--, col++;
-
-        // up 
-        while ((mat?.[row]?.[col] === DEFAULT_VALUE) && currNode) {
-            mat[row--][col] = currNode.val;
-            currNode = currNode.next;
-        }
-        row++, col++;
+        [i, j] = [newI, newJ];
+        currNode = currNode.next;
     }
 
     return mat;
