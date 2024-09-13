76d7c84bbec3650081d17904dcc703d49332b539
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-09-13 12:56:20 +0300

    Time: 90 ms (61.54%), Space: 65.2 MB (15.38%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.js
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.js
@@ -4,9 +4,13 @@
  * @return {number[]}
  */
 const xorQueries = function(arr, queries) {
-    return queries.map(([left, right]) => {
-        let queryResult = 0;
-        while (left <= right) queryResult ^= arr[left++];
-        return queryResult;
-    });
+    const xorMap = new Array(arr.length + 1).fill(0);
+
+    for (const [i, n] of arr.entries()) {
+        xorMap[i + 1] = xorMap[i] ^ n;
+    }
+
+    return queries.map(
+        ([left, right]) => xorMap[left] ^ xorMap[right + 1]
+    );
 };
