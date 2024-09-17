0485edecf40fc5032f4549faec732749d85a0944
Bahi_Habsh <167950824+BahiHabash@users.noreply.github.com>
2024-09-17 21:15:12 +0300

    Time: 40 ms (99.59%), Space: 50.5 MB (28.28%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.js
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.js
@@ -5,18 +5,16 @@
  */
 const uncommonFromSentences = function(s1, s2) {
     const [words1, words2] = [s1.split(' '), s2.split(' ')];
-    const [map1, map2] = [new Map(), new Map()];
+    const wordsFreq = new Map();
 
-    words1.forEach(word => map1.set(word, (map1.get(word) ?? 0) + 1));
-    words2.forEach(word => map2.set(word, (map2.get(word) ?? 0) + 1));
+    words1.forEach(word => wordsFreq.set(word, (wordsFreq.get(word) ?? 0) + 1));
+    words2.forEach(word => wordsFreq.set(word, (wordsFreq.get(word) ?? 0) + 1));
 
     let uncommonWords = [];
 
-    for (const [word, freq] of map1.entries())
-        if (freq === 1 && !map2.has(word)) uncommonWords.push(word);
-
-    for (const [word, freq] of map2.entries())
-        if (freq === 1 && !map1.has(word)) uncommonWords.push(word);
+    for (const [word, freq] of wordsFreq.entries()) {
+        if (freq === 1) uncommonWords.push(word);
+    }
 
     return uncommonWords;
 };
