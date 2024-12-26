55b6c19a74159e43589d7c2553d3de217a904b89
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2024-12-26 23:38:16 +0200

    Time: 0 ms (100%), Space: 50.4 MB (21.33%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -12,11 +12,10 @@
  */
 const invertTree = function(root) {
     if (!root) return root;
-    
+
     [root.left, root.right] = [root.right, root.left];
-    
     invertTree(root.left);
     invertTree(root.right);
-    
+
     return root;
 };
