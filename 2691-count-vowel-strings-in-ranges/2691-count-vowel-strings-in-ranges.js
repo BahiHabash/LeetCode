e84b26f7af8d21b7714349254945b005fa60ef5d
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-01-02 06:12:48 +0200

    Time: 27 ms (22.45%), Space: 77.5 MB (65.31%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.js
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.js
@@ -5,19 +5,20 @@
  */
 function vowelStrings (words, queries) {
     const isVowel = char => 'aeiou'.includes(char);
-    
+
     let count = 0;
-    const prefixVowels = new Map([[-1, count]]);
+    const prefixVowels = new Array(words.length).fill(0);
 
     for (const [i, word] of words.entries()) {
         if ( isVowel(word.at(0)) && isVowel(word.at(-1)) )
             count++;
 
-        prefixVowels.set(i, count);
+        prefixVowels[i] = count;
     }
 
+
     return queries.map(([start, end]) =>
-        prefixVowels.get(end) - prefixVowels.get(start - 1)
+        prefixVowels[end] - (prefixVowels[start - 1] ?? 0)
     );
 };
 
