592e7313caaefe6a2c0dd2da68779bfa0ec0543b
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-01-04 04:57:51 +0200

    Time: 310 ms (82.61%), Space: 61.9 MB (8.7%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.js
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.js
@@ -3,21 +3,21 @@
  * @return {number}
  */
 const countPalindromicSubsequence = function(s) {
-    const mapp = new Map();
-
+    // get min and max index for each char
+    const charIdxBoundries = new Map();
     for (let i = 0; i < s.length; i++) {
-        // Update min index for this char so far
-        if (!mapp.has(s[i])) {
-            mapp.set(s[i], [i, 0]);
+        if (!charIdxBoundries.has(s[i])) {
+            charIdxBoundries.set(s[i], [i, 0]);
         } else {
-            mapp.get(s[i])[1] = i;
+            charIdxBoundries.get(s[i])[1] = i;
         }
     }
 
+    // count all unique subsequences of length 3
     let count = 0;
-    for (const [char, [left, right]] of mapp.entries()) {
+    for (const [left, right] of charIdxBoundries.values()) {
         if ((right - left) >= 2) {
-            count += new Set( [...s.slice(left + 1, right)] ).size;
+            count += new Set([ ...s.slice(left + 1, right) ]).size;
         }
     }
 
