a9331068404d540abf0658b0b33c9f54aa524d18
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-01-06 05:16:14 +0200

    Time: 6 ms (70.87%), Space: 53.4 MB (34.95%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.js
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.js
@@ -3,17 +3,37 @@
  * @return {number[]}
  */
 const minOperations = function(boxes) {
-    const minOperations = new Array(boxes.length).fill(0);
+    let [leftOnes, rightOnes] = [0, 0];
+    let [leftCost, rightCost] = [0, 0];
 
-    for (let i = 0; i < boxes.length; i++) {
-        let minOperation = 0;
-
-        for (let j = 0; j < boxes.length; j++) {
-            if (boxes[j] === '1')
-                minOperation += Math.abs(i - j);
+    // Calc minOPeration for the first box
+    for (let i = 1; i < boxes.length; i++) {
+        if (boxes[i] === '1') {
+            rightOnes++;
+            rightCost += i;
         }
+    }
 
-        minOperations[i] = minOperation;
+    // calc the minOPeration for the remaining boxes
+    const minOperations = new Array(boxes.length).fill(0);
+    minOperations[0] = rightCost;
+
+    for (let i = 1; i < boxes.length; i++) {
+        // update right oprations
+        if (boxes[i] === '1') {
+            rightCost--;
+            rightOnes--;
+        }
+        rightCost -= rightOnes;
+
+        // update left operations
+        if (boxes[i - 1] === '1') {
+            leftOnes++;
+        }
+        leftCost += leftOnes;
+
+        // clac minOpearation for current box
+        minOperations[i] = leftCost + rightCost;
     }
 
     return minOperations;
