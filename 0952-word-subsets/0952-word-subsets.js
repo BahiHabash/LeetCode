43211644edc1c11b36fe4dd1606125ca58cd63fe
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-01-10 04:39:47 +0200

    Time: 82 ms (75%), Space: 72 MB (39.29%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.js
+++ 0952-word-subsets/0952-word-subsets.js
@@ -4,45 +4,36 @@
  * @return {string[]}
  */
 const wordSubsets = function(words1, words2) {
-    const words1Maps = words1.map(word => creatFreqMap(word));
-    const words2Map = new Map();
-
+    const words2FreqArr = new Array(26).fill(0);
+    // get the max number of char frequency for all words2
     for (const word of words2) {
-        const wordMap = creatFreqMap(word);
-
-        for (const [char, freq] of wordMap) {
-            words2Map.set(
-                char, Math.max(freq, words2Map.get(char) ?? -Infinity)
-            );
+        for (const [i, freq] of getFreqArr(word).entries()) {
+            words2FreqArr[i] = Math.max(freq, words2FreqArr[i]);
         }
     }
 
-    const universalStrings = [];
+    const universalStrings = []; // the output
 
-    for (const [i, word1Map] of words1Maps.entries()) {
-        let areSubsets = true;
+    for (const word1 of words1) {
+        const word1FreqArr = getFreqArr(word1);
+        const isUniversal = words2FreqArr.every((freq, i) => freq <= word1FreqArr[i]);
 
-        for (const [char, freq] of words2Map) {
-            if ((word1Map.get(char) ?? 0) < freq) {
-                areSubsets = false;
-                break;
-            }
-        }
-
-        if (areSubsets) {
-            universalStrings.push(words1[i]);
-        }
+        if (isUniversal) universalStrings.push(word1);
     }
 
     return universalStrings;
 };
 
-function creatFreqMap(word) {
-    const freqMap = new Map();
+/**
+ * @param word
+ * @return {Number[]} frequency character array
+ */
+function getFreqArr(word) {
+    const freqArr = new Array(26).fill(0);
 
     for (const char of word) {
-        freqMap.set(char, (freqMap.get(char) ?? 0) + 1);
+        freqArr[char.charCodeAt(0) - 97]++;
     }
 
-    return freqMap;
+    return freqArr;
 }
