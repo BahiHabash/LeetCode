4ed6fb9cfea2bf1bec2f292b48213ce28a0fa195
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-01-11 02:47:31 +0200

    Time: 11 ms (80%), Space: 52.6 MB (76%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.js
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.js
@@ -4,24 +4,22 @@
  * @return {boolean}
  */
 const canConstruct = function(s, k) {
+    // Handle edge Cases
     if (s.length < k) return false;
-    else if (s.length === k) return true;
+    if (s.length === k) return true;
 
+    // count chars' frequencies 
     const freqArr = new Array(26).fill(0);
     for (const char of s) {
         freqArr[char.charCodeAt(0) - 97]++;
     }
 
+    // count how many odds existed
     let odd = 0;
-    let evenPairs = 0;
-    for (let [i, freq] of freqArr.entries()) {
-        if (freq % 2 === 1) {
-            odd++;
-            freqArr[i] = --freq;
-        }
-
-        evenPairs += freq / 2;
+    for (const freq of freqArr) {
+        odd += (freq % 2 === 1);
     }
 
+    // no way to combine more than one odd in a single palindrome
     return k >= odd;
 };
