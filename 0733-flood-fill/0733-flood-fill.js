7f47cea5a0498ad78819cef308a8dd9dc27835c7
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-01-14 19:45:46 +0200

    Time: 1 ms (54.2%), Space: 53.4 MB (65.18%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -2,33 +2,31 @@
  * @param {number[][]} image
  * @param {number} sr
  * @param {number} sc
- * @param {number} color
+ * @param {number} newColor
  * @return {number[][]}
  */
-function floodFill(image, sr, sc, color) {
-    const visited = new Set();
-    const oldColor = image[sr][sc];
+function floodFill(image, sr, sc, newColor) {
+    const color = image[sr][sc];
     const DIRECTIONS = [[1, 0], [-1, 0], [0, 1], [0, -1]];
 
-    function isValid(image, r, c) {
-        return (r >= 0) && (r < image.length) &&
-               (c >= 0) && (c < image[0].length) &&
-               (image[r][c] === oldColor) && (!visited.has(r * image[0].length + c));
-    }
+    if (image[sr][sc] === newColor) return image;
 
-    const queue = [[sr, sc]];
+    function DFS(r, c) {   
+        image[r][c] = newColor;
 
-    while (queue.length) {
-        const [r, c] = queue.shift();
-        image[r][c] = color;
-
-        for (const [i, j] of DIRECTIONS){ 
-            if ( isValid(image, r+i, c+j) ){
-                queue.push([r+i, c+j]);
-                visited.add(r * image[0].length + c);
-            }
+        for (const [i, j] of DIRECTIONS) {
+            if (isValid(r+i, c+j)) 
+                DFS(r+i, c+j);
         }
     }
+
+    function isValid(r, c) {
+        return (r >= 0) && (r < image.length) &&
+               (c >= 0) && (c < image[0].length) &&
+               (image[r][c] === color)
+    }
+
+    DFS(sr, sc);
 
     return image;
 }
