8dfe5d45526d76cb177f08fc77fcca61f6c14eae
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-01-14 19:10:26 +0200

    Time: 2 ms (32.74%), Space: 54.1 MB (25.92%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -6,31 +6,36 @@
  * @return {number[][]}
  */
 function floodFill(image, sr, sc, color) { // Time: O(N), Spase: O(N)
+    // Generate Directions to move to
     const DIRECTIONS = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    // Create a visited Array
     const visited = new Array(image.length);
     for (let i = 0; i < visited.length; i++) {
         visited[i] = new Array(image[0].length).fill(0);
     }
 
-    DFS(image, sr, sc, image[sr][sc], color, visited, DIRECTIONS);
+    function DFS(image, sr, sc) {
+        if (visited[sr][sc]) return;
+
+        visited[sr][sc] = true;
+
+        for (const [i, j] of DIRECTIONS)
+            if ( isValid(image, sr + i, sc + j, image[sr][sc]) )
+                DFS(image, sr + i, sc + j);
+
+        image[sr][sc] = color;
+    }
+
+    function isValid(image, r, c, oldColor) {
+        return (r >= 0) && (r < image.length) &&
+               (c >= 0) && (c < image[0].length) &&
+               (image[r][c] === oldColor);
+    }
+
+    DFS(image, sr, sc);
 
     return image;
 }
 
-function DFS(image, sr, sc, color, newColor, visited, DIRECTIONS) {
-    if (visited[sr][sc] || image[sr][sc] !== color) return;
 
-    visited[sr][sc] = true;
-    image[sr][sc] = newColor;
-
-    for (const [i, j] of DIRECTIONS) {
-        if (isValid(image, sr + i, sc + j, color)) {
-            DFS(image, sr + i, sc + j, color, newColor, visited, DIRECTIONS);
-        }
-    }
-}
-
-function isValid(image, r, c, color) {
-    return (r >= 0) && (r < image.length) && (c >= 0) && (c < image[0].length) && (image[r][c] === color);
-}
 
