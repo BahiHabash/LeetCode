ba705dcdbaaed9229969f043db0055529dd85a6f
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-01-29 23:06:48 +0200

    Time: 0 ms (100%), Space: 48.4 MB (90.21%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.js
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.js
@@ -3,20 +3,5 @@
  * @return {boolean}
  */
 function isStrictlyPalindromic(n) {
-    for (let i = 2; i <= n - 2; i++) {
-        if ( !isPalandrome(n.toString(i)) ) 
-            return false;
-    }
-
-    return true;
-}
-
-function isPalandrome(str) {
-    let [i, j] = [0, str?.length - 1 ?? 0];
-
-    while (i < j) {
-        if (str[i++] !== str[j--]) return false;
-    }
-
-    return true;
+    return false;
 }
