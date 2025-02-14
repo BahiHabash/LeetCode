4c411ee2d77614c0210f0f038ff7124404682ff3
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-02-14 13:00:58 +0200

    Time: 45 ms (89.74%), Space: 78.3 MB (48.72%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.js
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.js
@@ -1,32 +1,42 @@
-
 class ProductOfNumbers {
+    #prefixProduct;
+    #prefixZeroCount;
+
     constructor() {
         this.stream = [];
+        this.#prefixProduct = [1]; // Initialize with 1 to handle the first product correctly
+        this.#prefixZeroCount = [0]; // Initialize with 0 to handle the first zero count correctly
     }
 
     /** 
-    * @param {number} num
-    * @return {void}
-    */
+     * @param {number} num
+     * @return {void}
+     */
     add(num) {
+        // add num to the stream
         this.stream.push(num);
+        // update prefixProduct for the current index
+        this.#prefixProduct.push((this.#prefixProduct.at(-1) * num) || 1);
+        // update prefixZeroCount for the current index
+        this.#prefixZeroCount.push(this.#prefixZeroCount.at(-1) + (num === 0));
     }
 
     /** 
-    * @param {number} k
-    * @return {number}
-    */
+     * @param {number} k
+     * @return {number}
+     */
     getProduct(k) {
-        let product = 1;
-
-        for (let i = this.stream.length - k; i < this.stream.length; i++) {
-            product *= this.stream[i];
+        const len = this.stream.length;
+        
+        // if there is any zero in the last k integers, return 0
+        if (this.#prefixZeroCount[len] - this.#prefixZeroCount[len - k]) {
+            return 0;
+        } else {
+            // return the product of the last k numbers
+            return this.#prefixProduct[len] / this.#prefixProduct[len - k];
         }
-
-        return product;
     }
 }
-
 
 /** 
  * Your ProductOfNumbers object will be instantiated and called as such:
