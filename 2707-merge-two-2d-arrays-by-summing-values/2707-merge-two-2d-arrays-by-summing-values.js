f627259a95f08381f6e6790256141e773b1be1df
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-03-03 03:57:00 +0200

    Time: 9 ms (22.67%), Space: 60.6 MB (10.67%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.js
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.js
@@ -6,38 +6,12 @@
 function mergeArrays(nums1, nums2) {
     const idsToValue = new Map();
 
-    let [i, j] = [0, 0];
-
-    while ((i < nums1.length) && (j < nums2.length)) {
-        if (nums1[i][0] < nums2[j][0]) {
-            idsToValue.set(
-                nums1[i][0]
-                , (idsToValue.get(nums1[i][0]) ?? 0) + nums1[i][1]
-            )
-            i++;
-        } else {
-            idsToValue.set(
-                nums2[j][0]
-                , (idsToValue.get(nums2[j][0]) ?? 0) + nums2[j][1]
-            )
-            j++;
-        }
+    for (const [id, val] of nums1) {
+        idsToValue.set(id, val);
     }
 
-    while (i < nums1.length) {
-        idsToValue.set(
-            nums1[i][0]
-            , (idsToValue.get(nums1[i][0]) ?? 0) + nums1[i][1]
-        )
-        i++;
-    }
-
-    while (j < nums2.length) {
-        idsToValue.set(
-            nums2[j][0]
-            , (idsToValue.get(nums2[j][0]) ?? 0) + nums2[j][1]
-        )
-        j++;
+    for (const [id, val] of nums2) {
+        idsToValue.set(id, (idsToValue.get(id) ?? 0) + val);
     }
 
     return [...idsToValue.entries()].sort((a, b) => a[0] - b[0]);
