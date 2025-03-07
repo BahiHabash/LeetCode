125d308ceb270476f2ac5d1d756899cfe92a7ac3
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-03-07 21:26:48 +0200

    Time: 17 ms (5.56%), Space: 76.8 MB (5.08%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -3,34 +3,18 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-const rotate = function(nums, k) { // Time : O(n), Space (1)   
-    const len = nums.length;
-    k %= len;
-    
-    if (k === 0)
-        return;
-    
-    let pivot = len - k;
-    reverse(nums, 0, pivot - 1);
-    reverse(nums, pivot, len - 1);
-    reverse(nums, 0, len - 1); // Reverses the entire array
-};
+var rotate = function(nums, k) {
+    const idxToNum = new Map(nums.entries());
 
-/**
- * Reverses the elements in the given array within the specified range.
- * If no range is provided, the entire array is reversed.
- * @param {number[]} nums - The array to reverse.
- * @param {number} [left=0] - The starting index of the range to reverse.
- * @param {number} [right=nums.length - 1] - The ending index of the range to reverse.
- * @returns {number[]} The array after the reversal operation.
- */
-const reverse = function(nums, left = 0, right = nums.length - 1) {
-    if (!nums || left > right || right >= nums.length)   
-        return nums;
-    
-    while (left < right) {
-        [nums[left], nums[right]] = [nums[right], nums[left]];
-        left++, right--;
+    k = k % (nums.length);
+    let idx = nums.length - k;
+
+    for (let i = 0; i < nums.length; i++) {
+        // get element
+        nums[i] = idxToNum.get( (idx % nums.length) );
+
+        // update idx
+        idx++;
     }
 
     return nums;
