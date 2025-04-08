c6182e612dcb5c68d38a1601fb1d166d1340cca9
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-04-08 22:44:15 +0200

    Time: 1 ms (96%), Space: 57.7 MB (72%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.ts
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.ts
@@ -1,25 +1,13 @@
 function minimumOperations(nums: number[]): number {
-    let numOfDublicatedNums: number = 0;
-    const numsFreq: number[] = new Array(101).fill(0);
+    let seen = new Set();
 
-    for (let i = 0; i < nums.length; i++) {
-        numsFreq[nums[i]]++;
-        if (numsFreq[nums[i]] === 2) numOfDublicatedNums++;
-    }
-    
-    
-    let minOperations: number = 0;
-
-    for (let i = 0; i < nums.length; i += 3) {
-        if (numOfDublicatedNums === 0) break;
-
-        for (let j = i; j < Math.min(nums.length, i + 3); j++) {
-            numsFreq[nums[j]]--;
-            if (numsFreq[nums[j]] === 1) numOfDublicatedNums--;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (seen.has(nums[i])) {
+            return Math.ceil((i + 1) / 3);
+        } else {
+            seen.add(nums[i]);
         }
-
-        minOperations++;
     }
 
-    return minOperations;
+    return 0;
 };
