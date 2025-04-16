594760257929f12662e4bcc1094bc6a95a883568
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-04-16 14:07:01 +0200

    Time: 623 ms (11.02%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -3,9 +3,10 @@ SELECT
     a.machine_id,
     ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
 FROM
-    Activity a,
+    Activity a
+JOIN
     Activity b
-WHERE
+ON
     a.machine_id = b.machine_id
     AND
     a.process_id = b.process_id
