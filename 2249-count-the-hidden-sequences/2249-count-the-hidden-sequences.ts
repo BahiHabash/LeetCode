01f71c3fb67e2ae71c506437e18114617c40db88
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-04-21 04:30:48 +0200

    Time: 12 ms (0%), Space: 73.3 MB (25%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.ts
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.ts
@@ -1,43 +1,16 @@
 function numberOfArrays(differences: number[], lower: number, upper: number): number {
-    let maxDiff: number = -Infinity;
-    let maxDiffIdx: number = -1;
+    let currSumDiff: number = 0;
+    let min: number = 0;
+    let max: number = 0;
 
-    // Get max difference index
-    for (let i = 0; i < differences.length; i++) {
-        if (Math.abs(differences[i]) > maxDiff) {
-            maxDiff = Math.abs(differences[i]);    
-            maxDiffIdx = i;
-        }
+    for (const diff of differences) {
+        currSumDiff += diff;
+        min = Math.min(min, currSumDiff);
+        max = Math.max(max, currSumDiff);
     }
-
-    if ((lower + maxDiff) > upper) return 0;
-
-    // get the Boundries or the Range of numbers that may form the original array
-    let num: number = lower;
-    let min: number = num;
-    let max: number = num;
-
-    // apply the differences to the right of maxDiffIdxc
-    for (let i = maxDiffIdx; i < differences.length; i++) {
-        num += differences[i];
-        min = Math.min(num, min);
-        max = Math.max(num, max);
-    }
-
-    // reset the initial value at maxDiffIdx
-    num = lower;
-
-    // apply the differences to the left of maxDiffIdx
-    for (let i = maxDiffIdx; i > 0; i--) {
-        num -= differences[i - 1];
-        min = Math.min(num, min);
-        max = Math.max(num, max);
-    }
-
-    const originalNumbersRange: number = max - min;
 
     return Math.max(
         0, 
-        (upper - lower) - originalNumbersRange + 1
+        (upper - lower) - ( max - min) + 1
     );
 }
