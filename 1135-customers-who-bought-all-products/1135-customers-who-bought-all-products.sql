60466b5199c4c6c1e5c86bfb28fda8ec5f65fd17
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-04-21 05:26:38 +0200

    Time: 608 ms (68.56%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -2,4 +2,7 @@
 SELECT C.customer_id
 FROM Customer C
 GROUP BY C.customer_id
-HAVING COUNT(DISTINCT C.product_key) = (SELECT COUNT(*) FROM Product);
+HAVING COUNT(DISTINCT C.product_key) = (
+        SELECT COUNT(DISTINCT product_key) 
+        FROM Product
+    );
