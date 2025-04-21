61176852ae79671a39a64d9b8ed509a40b75b376
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-04-21 02:45:10 +0200

    Time: 1 ms (100%), Space: 56.5 MB (100%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.ts
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.ts
@@ -1,12 +1,16 @@
 function numRabbits(answers: number[]): number {
-    const ansFreq = new Array<number>(1000).fill(0);
+    const ansFreq = {};
 
-    for (const ans of answers) ansFreq[ans]++;
+    for (const ans of answers) {
+        ansFreq[ans] = (ansFreq[ans] ?? 0) + 1;
+    }
 
     let minRabbits: number = 0;
     
-    for (const [ans, freq] of ansFreq.entries()) {
-        minRabbits += Math.ceil(freq / (ans + 1)) * (ans + 1);
+    for (const ans in ansFreq) {
+        const rabbitsPerGroup: number = +ans + 1;
+        const numOfGroups: number = Math.ceil(ansFreq[ans] / rabbitsPerGroup);
+        minRabbits += numOfGroups * rabbitsPerGroup;
     }
 
     return minRabbits;
