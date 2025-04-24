1a37de1c89435736a555d1309e9c0ef2c36b9002
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-04-24 03:25:17 +0200

    Time: 199 ms (37.5%), Space: 60.8 MB (100%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.ts
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.ts
@@ -1,37 +1,16 @@
 function countCompleteSubarrays(nums: number[]): number {
-    // at least there is one complete array
-    let numOfCompleteSubarrays: number = 1;
-    const numOfDistinctNums: number = new Set<number>(nums).size;
+    const totalDistinct = new Set(nums).size;
+    let count = 0;
 
-    for (let i = numOfDistinctNums; i < nums.length; i++) {
-        const currWindow = new Map<number, number>();
-
-        // initial the window
-        for (let j = 0; j < i; j++) {
-            currWindow.set(nums[j], (currWindow.get(nums[j]) ?? 0) + 1);
-        }
-        // update numOfCompleteSubarrays
-        if (currWindow.size === numOfDistinctNums) {
-            numOfCompleteSubarrays++;
-        }
-
-        // slide the window
-        for (let j = i; j < nums.length; j++) {
-            // remove left most element
-            currWindow.set(nums[j - i], (currWindow.get(nums[j - i]) ?? 0) - 1);
-            // update numOfCompleteSubarrays
-            if (currWindow.get(nums[j - i]) === 0) {
-                currWindow.delete(nums[j - i]);
-            }
-            
-            // add right most element
-            currWindow.set(nums[j], (currWindow.get(nums[j]) ?? 0) + 1);
-            // update numOfCompleteSubarrays
-            if (currWindow.size === numOfDistinctNums) {
-                numOfCompleteSubarrays++;
+    for (let start = 0; start < nums.length; start++) {
+        const seen = new Set<number>();
+        for (let end = start; end < nums.length; end++) {
+            seen.add(nums[end]);
+            if (seen.size === totalDistinct) {
+                count++;
             }
         }
     }
 
-    return numOfCompleteSubarrays;
-};
+    return count;
+}
