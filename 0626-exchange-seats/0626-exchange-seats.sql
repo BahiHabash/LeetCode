4fa6451bd155b7ae63e39b7d62722fc7031e9b1e
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-04-26 11:34:50 +0300

    Time: 343 ms (83.76%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,9 +1,8 @@
 # Write your MySQL query statement below
 SELECT 
     CASE 
-        WHEN id % 2 = 1 AND id + 1 IN (SELECT id FROM Seat) THEN id + 1
         WHEN id % 2 = 0 THEN id - 1
-        ELSE id
+        ELSE IF(id + 1 IN (SELECT id FROM Seat), id + 1, id)
     END AS id,
     student
 FROM Seat
