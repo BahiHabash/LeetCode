1ba051a4b47789db3739d137ffc2a1920e869e0b
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-05-02 08:58:34 +0300

    Time: 476 ms (35.71%), Space: 76 MB (50%) - LeetHub

--- 0868-push-dominoes/0868-push-dominoes.ts
+++ 0868-push-dominoes/0868-push-dominoes.ts
@@ -1,52 +1,30 @@
 function pushDominoes(dominoes: string): string {
-    let dominoesArrGlobal: string[] = dominoes.split('');
+    let dominoesArr: string[] = dominoes.split('');
     
-    for (let j = 0; j < dominoesArrGlobal.length; j++) {
-        const dominoesArr = [...dominoesArrGlobal];
+    while (true) {
         let somethingChanged: boolean = false;
+        const str: string = dominoesArr.join('');
 
-        for (let i = 1; i < dominoesArr.length - 1; i++) {
-            if (dominoesArrGlobal[i] === '.') {
-                const str: string = dominoesArrGlobal[i - 1] + dominoesArrGlobal[i + 1];
-
-                if ( ['LR', 'RL', '.R', 'L.', '..'].includes(str) ) {
-                    continue;
-                } else if ( ['.L', 'LL'].includes(str) ) {
-                    dominoesArr[i] = 'L';
-                } else if ( ['R.', 'RR'].includes(str) ) { 
-                    dominoesArr[i] = 'R';
-                }
-
+        for (let i = 0; i < dominoesArr.length; i++) {
+            if (
+                str[i] === 'L' && 
+                str[i - 1] === '.' && 
+                str[i - 2] !== 'R'
+            ) {
+                dominoesArr[i - 1] = 'L';
+                somethingChanged = true;
+            } else if (
+                str[i] === 'R' &&
+                str[i + 1] === '.' && 
+                str[i + 2] !== 'L'
+            ) {
+                dominoesArr[i + 1] = 'R';
                 somethingChanged = true;
             }
         }
 
-        dominoesArrGlobal = [...dominoesArr];
         if (!somethingChanged) break;
     }
 
-    const leftMostMove: string = dominoesArrGlobal.find(move => move !== '.');
-    if (leftMostMove === 'L') {
-        for (let i = 0; i < dominoesArrGlobal.length; i++) {
-            if (dominoesArrGlobal[i] !== '.') break;
-            dominoesArrGlobal[i] = leftMostMove;
-        }
-    }
-
-    let rightMostMove: string;
-    for (let i = dominoesArrGlobal.length - 1; i >= 0; i--) {
-        if (dominoesArrGlobal[i] !== '.') {
-            rightMostMove = dominoesArrGlobal[i];
-            break;
-        }
-    }
-
-    if (rightMostMove === 'R') {
-        for (let i = dominoesArrGlobal.length - 1; i >= 0; i--) {
-            if (dominoesArrGlobal[i] !== '.') break;
-            dominoesArrGlobal[i] = rightMostMove;
-        }
-    }
-
-    return dominoesArrGlobal.join('');
+    return dominoesArr.join('');
 }
