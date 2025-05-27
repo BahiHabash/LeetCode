d65ec434113fb99273ee3e5051958a3d170b73b9
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-05-27 22:47:58 +0300

    Time: 1 ms (32.76%), Space: 56.1 MB (87.93%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.ts
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.ts
@@ -1,13 +1,8 @@
 function differenceOfSums(n: number, m: number): number {
     let sumDivisible: number = 0;
-    let sumNotDivisible: number = 0;
+    for (let i = m; i <= n; i += m) sumDivisible += i;
 
-    for (let i = 1; i <= n; i++) {
-        if (i % m === 0) 
-            sumDivisible += i;
-        else 
-            sumNotDivisible += i;
-    }
+    const sumNotDivisible: number = ((n * (n + 1)) / 2) - sumDivisible;
 
     return sumNotDivisible - sumDivisible;
 };
