dd8b351aac4e121b9bb9d68ebc3b5e63fab6faa9
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-06-08 05:35:55 +0300

    Time: 9 ms (21.05%), Space: 63 MB (42.11%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.ts
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.ts
@@ -1,14 +1,19 @@
 function lexicalOrder(n: number): number[] {
-    const arr: number[] = new Array(n);
+    const arr: number[] = [];
 
-    for (let i = 0; i < n; i++) {
-        arr[i] = i + 1;
+    for (let i = 1; i <= 9; i++) {
+        DFS(n, arr, i);
     }
 
-    arr.sort((a, b) => {
-        if (String(a) < String(b)) return -1;
-        else return 1;
-    });
-
     return arr;
+};
+
+function DFS(n: number, arr: number[], curr: number): void {
+    if (curr > n) return;
+
+    arr.push(curr);
+
+    for (let i = 0; i <= 9; i++) {
+        DFS(n, arr, curr * 10 + i);
+    }
 };
