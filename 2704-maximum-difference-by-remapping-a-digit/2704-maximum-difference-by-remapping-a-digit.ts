6d90d2658ec6b27d987c413389b73a750b2edd94
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-06-15 16:34:32 +0300

    Time: 1 ms (43.75%), Space: 55.6 MB (31.25%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.ts
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.ts
@@ -7,21 +7,14 @@ function minMaxDifference(num: number): number {
 
 function getMaxNum(num: number): number {
     const numString: string = String(num);
-    let maxRemapedDigit: string = '9';
     let maxNum: string = '';
 
     for (const d of numString) {
-        if (d !== '9') {
-            maxRemapedDigit = d;
-            break;
-        }
-    }
-
-    for (const d of numString) {
-        if (d === maxRemapedDigit) {
+        if (d === '9') {
             maxNum += '9';
         } else {
-            maxNum += d;
+            maxNum = numString.replaceAll(d, '9');
+            break;
         }
     }
 
@@ -30,17 +23,7 @@ function getMaxNum(num: number): number {
 
 function getMinNum(num: number): number {
     const numString: string = String(num);
-
-    const minRemapedDigit: string = numString[0];
-    let minNum: string = '';
-
-    for (const d of numString) {
-        if (d === minRemapedDigit) {
-            minNum += '0';
-        } else {
-            minNum += d;
-        }
-    }
+    const minNum: string = numString.replaceAll(numString[0], '0');
 
     return Number(minNum);
 };
