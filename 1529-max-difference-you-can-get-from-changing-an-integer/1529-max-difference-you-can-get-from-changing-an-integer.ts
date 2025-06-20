4b3f38b0869a1676cb5e2cf230000729ad8e2969
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-06-20 15:33:24 +0300

    Time: 1 ms (52.52%), Space: 56.2 MB (27.16%) - LeetHub

--- 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.ts
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.ts
@@ -1,9 +1,14 @@
 function maxDiff(num: number): number {
+    const max: number = getMax(num);
+    const min: number = getMin(num);
+
+    return max - min;
+};
+
+function getMax(num: number): number {
     const numString: string = String(num);
     let max: string = '';
-    let min: string = numString;
 
-    // construct the maximum possible number
     for (const d of numString) {
         if (d === '9') {
             max += d;
@@ -13,19 +18,24 @@ function maxDiff(num: number): number {
         }
     }
 
-    // construct the minimum possible number
-    if (numString[0] === '1') {
-        for (let i = 1; i < numString.length; i++) {
-            const d: string = numString[i];
+    return Number(max);
+};
 
-            if (d !== '0' && numString.replaceAll(d, '0')[0] !== '0') {
-                min = numString.replaceAll(d, '0');
-                break;
+function getMin(num: number): number {
+    const numString: string = String(num);
+    let min: string = numString.replaceAll(numString[0], '1');
+    
+    for (const d of numString) {
+        if (
+            d !== '0' && 
+            numString.replaceAll(d, '0')[0] !== '0'
+        ) {
+            const newMin: string = numString.replaceAll(d, '0');
+            if (newMin < min) {
+                min = newMin;
             }
         }
-    } else {
-        min = numString.replaceAll(numString[0], '1');
     }
-    
-    return Number(max) - Number(min);
+
+    return Number(min);
 };
