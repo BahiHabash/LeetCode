8c27d8541c883eff9f15b2a1cffd94cda51a86a5
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2025-07-04 00:23:12 +0300

    Time: 7 ms (25%), Space: 60.5 MB (16.18%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.ts
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.ts
@@ -1,19 +1,13 @@
-function kthCharacter(k: number): string {
-    let word: string = 'a';
-
-    while (word.length < k) {
-        word = updateWord(word);
+function kthCharacter(k: number, word: string = 'a'): string {
+    if (word.length >= k) {
+        return word[k - 1];
     }
-    
-    return word[k - 1];
-}
 
-function updateWord(word: string): string {
     let appendedWord: string = '';
 
     for (const char of word) {
         appendedWord += String.fromCharCode( ((char.charCodeAt(0) + 1) % 97) + 97 );
     }
-
-    return word + appendedWord;
+    
+    return kthCharacter(k, word + appendedWord);
 }
