42b92a1e9d6226c43f1d585035d9ebb93096c6a7
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2026-01-12 17:42:15 +0200

    Time: 2 ms (46.3%), Space: 56.9 MB (75.93%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.ts
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.ts
@@ -1,34 +1,21 @@
 function minTimeToVisitAllPoints(points: number[][]): number {
     let minTime: number = 0;
-    const curr: [number, number] = [points[0][0], points[0][1]];
+    let curr: number[] = points[0];
 
     for (let i = 1; i < points.length; i++) {
-        console.log(`minTime: `, minTime);
-        console.log(`curr: `, curr);
-        console.log(`Points: `, points[i]);
-        console.log('==========================');
+        const curr: number[] = points[i - 1];
+        const target: number[] = points[i];
 
+        const dX: number = Math.abs(target[0] - curr[0]);  // delta x diagonal
+        const dY: number = Math.abs(target[1] - curr[1]);  // delta y diagonal
 
-
-        const minX: number = Math.abs(points[i][0] - curr[0]);
-        const minY: number = Math.abs(points[i][1] - curr[1]);
-
-        const minMoves: number = Math.min(minX, minY);
-        minTime += minMoves;
-
-        curr[0] += (curr[0] < points[i][0]) ? minMoves : -minMoves;
-        curr[1] += (curr[1] < points[i][1]) ? minMoves : -minMoves;
-
-        if (curr[0] === points[i][0]) {
-            const moves: number = Math.abs(points[i][1] - curr[1]);
-            minTime += moves;
-        } else if (curr[1] === points[i][1]) {
-            const moves: number = Math.abs(points[i][0] - curr[0]);
-            minTime += moves;
+        if (dX < dY) {
+            const extraYMoves: number = Math.abs(target[1] - curr[1]) - dX;
+            minTime += dX + extraYMoves;
+        } else {
+            const extraYMoves: number = Math.abs(target[0] - curr[0]) - dY;
+            minTime += dY + extraYMoves;
         }
-
-        curr[0] = points[i][0];
-        curr[1] = points[i][1];
     }
 
     return minTime;
