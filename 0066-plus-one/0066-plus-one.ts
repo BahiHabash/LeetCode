8b308b14ac90daa9989a734013902cdff36afbbd
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2026-01-13 19:49:13 +0200

    Time: 0 ms (100%), Space: 55.4 MB (51.68%) - LeetHub

--- 0066-plus-one/0066-plus-one.ts
+++ 0066-plus-one/0066-plus-one.ts
@@ -1,14 +1,16 @@
 function plusOne(digits: number[]): number[] {
     for (let i = digits.length - 1; i >= 0; i--) {
-        if (digits[i] === 9) {
-            digits[i] = 0;
-        } else {
+        if (digits[i] < 9) {
             digits[i]++;
-            return digits;
+            break;
+        } else {
+            digits[i] = 0;
         }
     }
 
-    digits.unshift(1);
+    if (digits[0] === 0) {
+        digits.unshift(1);
+    }
 
     return digits;
 };
