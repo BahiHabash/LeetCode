e0f1de672b6c830304a9a187ebbc49bc3e9aab06
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2026-01-18 21:46:22 +0200

    Time: 21 ms (36.29%), Space: 62.3 MB (5.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,9 +1,27 @@
 function twoSum(nums: number[], target: number): number[] {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
+    const numCountAndIndex: Record<number, { count: number; index: number[] }> = {};
+    
+    for (const [i, num] of nums.entries()) {
+        if (numCountAndIndex[num] === undefined) {
+            numCountAndIndex[num] = {
+                count: 0,
+                index: [],
             }
+        }
+
+        numCountAndIndex[num].count++;
+        numCountAndIndex[num].index.push(i);
+    }
+
+    if (numCountAndIndex[target / 2]?.count === 2) {
+        return numCountAndIndex[target / 2].index;
+    }
+
+    for (const num of Object.keys(numCountAndIndex)) {
+        const complement: number = target - Number(num);
+
+        if (numCountAndIndex[complement]) {
+            return [numCountAndIndex[complement].index[0], numCountAndIndex[num].index[0]];
         }
     }
 };
