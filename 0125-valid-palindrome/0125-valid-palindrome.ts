3041a7357e21ac650093130093354470ff322281
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2026-01-21 23:38:53 +0200

    Time: 7 ms (34.82%), Space: 60 MB (43.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,24 +1,41 @@
 function isPalindrome(s: string): boolean {
-    let str: string = '';
-    let reverseStr: string = '';
+    const len: number = s.length;
+    let l: number = 0;
+    let r: number = len - 1;
 
-    for (let i = 0; i < s.length; i++) {
-        if (isAlpha(s[i])) {
-            str += s[i].toLowerCase();
-        }   
+    while (l < r) {
+        while (l < len && !isAlphaNum(s[l])) {
+            l++;
+        }
+
+        while (r >= 0 && !isAlphaNum(s[r])) {
+            r--;
+        }
+
+        if (l >= len && r < 0) {
+            return true;
+        }
+
+        if (l >= len || r < 0) {
+            return false;
+        }
+
+        if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+            return false;
+        }
+
+        l++, r--;
     }
 
-    for (let i = s.length - 1; i >= 0; i--) {
-        if (isAlpha(s[i])) {
-            reverseStr += s[i].toLowerCase();
-        }        
-    }
-    
-    return str === reverseStr;
+    return true;
 };
 
-function isAlpha(char): boolean {
-    const charCode: number = char.charCodeAt();
+function isAlphaNum(char): boolean {
+    if ('0123456789'.includes(char)) {
+        return char;
+    }
+
+    const charCode: number = char.charCodeAt(0);
 
      return (charCode >= 'A'.charCodeAt(0) && charCode <= 'Z'.charCodeAt(0)) 
         ||  (charCode >= 'a'.charCodeAt(0) && charCode <= 'z'.charCodeAt(0));
