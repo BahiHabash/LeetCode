13b5b20ee0f1b9c9bf1d75f1682aff4676ad0c40
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2026-01-21 23:05:40 +0200

    Time: 2 ms (48.33%), Space: 57.9 MB (52.71%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.ts
+++ 0069-sqrtx/0069-sqrtx.ts
@@ -1,13 +1,29 @@
 function mySqrt(x: number): number {
-    for (let i = 0; i <= x; i++) {
-        const product: number = i * i;
+    if (x === 0 || x === 1) {
+        return x;
+    }
 
-        if (product === x) {
-            return i;
-        }
+    let l: number = 0;
+    let r: number = x;
+
+    while (Math.log2(x) + 1) {
+        const mid: number = Math.floor((l + r) / 2);
+        const product: number = mid * mid;
         
-        if (product > x) {
-            return i - 1;
+        if (product === x) {
+            return mid;
+        } else if (product > x) {
+            if ((mid - 1) ** 2 < x) {
+                return mid - 1;
+            }
+
+            r = mid - 1;
+        } else {
+            if ((mid + 1) ** 2 > x) {
+                return mid;
+            }
+
+            l = mid + 1;
         }
     }
 };
