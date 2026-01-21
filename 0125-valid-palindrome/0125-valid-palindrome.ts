b88f663946f65b07ef2f9347b65fbb94b0c8ca52
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2026-01-21 23:55:24 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,42 +1,7 @@
 function isPalindrome(s: string): boolean {
-    const len: number = s.length;
-    let l: number = 0;
-    let r: number = len - 1;
+    const chars: string[] = [...s]
+        .filter((c) => c.match(/\w/))
+        .map(c => c.toLowerCase());
 
-    while (l < r) {
-        while (l < len && !isAlphaNum(s[l])) {
-            l++;
-        }
-
-        while (r >= 0 && !isAlphaNum(s[r])) {
-            r--;
-        }
-
-        if (l >= len && r < 0) {
-            return true;
-        }
-
-        if (l >= len || r < 0) {
-            return false;
-        }
-
-        if (s[l].toLowerCase() !== s[r].toLowerCase()) {
-            return false;
-        }
-
-        l++, r--;
-    }
-
-    return true;
-};
-
-function isAlphaNum(char): boolean {
-    if ('0123456789'.includes(char)) {
-        return char;
-    }
-
-    const charCode: number = char.charCodeAt(0);
-
-     return (charCode >= 'A'.charCodeAt(0) && charCode <= 'Z'.charCodeAt(0)) 
-        ||  (charCode >= 'a'.charCodeAt(0) && charCode <= 'z'.charCodeAt(0));
+    return chars.join('') === chars.reverse().join('');
 };
