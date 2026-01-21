58a71f5809660d1b79ab860ed55c781955bb5026
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2026-01-22 00:13:35 +0200

    Time: 16 ms (43.17%), Space: 61.8 MB (13.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,3 +1,21 @@
 function isAnagram(s: string, t: string): boolean {
-    return [...s].sort().join('') === [...t].sort().join('');
+    const charsCount = new Map<string, number>();
+
+    for (const char of s) {
+        charsCount.set(char, (charsCount.get(char) || 0) + 1);
+    }
+
+    for (const char of t) {
+        const currCount: number = charsCount.get(char);
+
+        if (!currCount) {
+            return false;
+        } else if (currCount === 1) {
+            charsCount.delete(char);
+        } else {
+            charsCount.set(char, currCount - 1);
+        }
+    }
+
+    return charsCount.size === 0;
 };
