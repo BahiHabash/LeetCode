5efe3409dafe33f3ba72c2b305ace9a518735311
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2026-01-22 00:01:28 +0200

    Time: 8 ms (25.88%), Space: 67.5 MB (5.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,6 +1,6 @@
 function isPalindrome(s: string): boolean {
     const chars: string[] = [...s]
-        .filter((c) => c.match(/\w/))
+        .filter((c) => c.match(/\w/) && (c !== '_'))
         .map(c => c.toLowerCase());
 
     return chars.join('') === chars.reverse().join('');
