9edd3823931026b33b0d0dd2e00ddd477559d2a8
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2026-01-26 05:16:51 +0200

    Time: 79 ms (5.76%), Space: 65.4 MB (85.97%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+    let lca: TreeNode | null = null;
+
+    function dfs(root: TreeNode | null): void {
+        if (root) {
+            if (p.val === root.val || q.val === root.val) { // any val match current
+                lca = root;
+            } else if (Math.max(p.val, q.val) < root.val) { // both are greater than current
+                dfs(root.left);
+            } else if (Math.min(p.val, q.val) > root.val) { // both are less than current
+                dfs(root.right);
+            } else { // current in the middle
+                lca = root;
+            }
+        }     
+    }
+
+    dfs(root);
+
+    return lca;
+};
