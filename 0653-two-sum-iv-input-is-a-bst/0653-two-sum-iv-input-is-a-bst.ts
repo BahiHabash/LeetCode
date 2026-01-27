df99975214fe71c878bff356b47c8161f0c05ef3
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2026-01-27 06:09:28 +0200

    Time: 2 ms (78.08%), Space: 63.7 MB (98.63%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.ts
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function findTarget(root: TreeNode | null, k: number): boolean {
+    const seen = new Set();
+    let founded: boolean = false;
+
+    function dfs(root) {
+        if (!root || founded) {
+            return;
+        }
+
+        if (seen.has(k - root.val)) {
+            founded = true;
+            return;
+        }
+
+        seen.add(root.val);
+
+        dfs(root.left);
+        dfs(root.right);
+    }
+
+    dfs(root);
+
+    return founded;
+};
