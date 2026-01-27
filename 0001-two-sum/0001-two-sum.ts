0183514c404c10754dfdfc084f4c11fb75eed781
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2026-01-27 06:00:48 +0200

    Time: 5 ms (44.45%), Space: 60.4 MB (5.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,13 +1,13 @@
 function twoSum(nums: number[], target: number): number[] {
-    const seen = new Map<number, number>();
-    
-    for (const [i, num] of nums.entries()) {
+    const numIdx = new Map<number, number>();
+
+    for (const [idx, num] of nums.entries()) {
         const complement: number = target - num;
 
-        if (seen.has(complement)) {
-            return [i, seen.get(complement)];
+        if (numIdx.has(complement)) {
+            return [numIdx.get(complement), idx];
+        } else {
+            numIdx.set(num, idx);
         }
-
-        seen.set(num, i);
     }
 };
