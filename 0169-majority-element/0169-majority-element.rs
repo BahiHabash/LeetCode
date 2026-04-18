a1055a085a9fd298d0e7de35ed5fa39d6aaa89ea
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2026-04-18 02:11:30 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.rs
+++ 0169-majority-element/0169-majority-element.rs
@@ -1,6 +1,23 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn majority_element(mut nums: Vec<i32>) -> i32 {
-        nums.sort_unstable();
-        nums[nums.len() / 2]
+        let mut num_freq: HashMap<i32, i32> = HashMap::new();
+
+        for num in nums {
+            *num_freq.entry(num).or_insert(0) += 1;
+        }
+
+        let mut majority_num: i32 = 0;
+        let mut max_freq: i32 = 0;
+
+        for (&num, &freq) in num_freq.iter() {
+            if freq > max_freq {
+                max_freq = freq;
+                majority_num = num;
+            } 
+        }
+
+        majority_num
     }
 }
