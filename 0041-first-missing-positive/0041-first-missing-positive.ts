6d5ae21a9f38b1c610302995d7d081a1a72ab088
Mohammed Habash <167950824+BahiHabash@users.noreply.github.com>
2026-05-16 15:44:32 +0300

    Time: 31 ms (19.05%), Space: 76 MB (12.3%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.ts
+++ 0041-first-missing-positive/0041-first-missing-positive.ts
@@ -1,21 +1,12 @@
 function firstMissingPositive(nums: number[]): number {
-    nums.sort((a, b) => a - b);
+    let numsSet = new Set(nums);
+    let max = Math.max(...nums);
 
-    let i = 0;
-    let ans = 1;
-
-    while ((i < nums.length) && (nums[i] <= 1)) {
-        i++;
-    }
-
-    while (i < nums.length) {
-        let prev = nums[i - 1] ?? 0;
-        if (nums[i] - prev > 1) {
-            return Math.max(1, prev + 1);
+    for (let i = 1; i <= max + 1; i++) {
+        if (!numsSet.has(i)) {
+            return i;
         }
-
-        i++;
     }
 
-    return Math.max(1, nums.at(-1) + 1);
+    return 1;
 };
